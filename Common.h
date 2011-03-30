d9322541aebbdb65331f7559647727e1574573af
Tyler Folsom <tyler@tfolsom.com>
2011-03-30 17:41:22 +0000

    Added data structures and comments to Planner and Common.h

--- Common.h
+++ Common.h
@@ -2,6 +2,7 @@
 
 // value if latitude, longitude or bearing is missing.
 #define INVALID 1000
+
 struct waypoint
 {
     double latitude;
@@ -15,7 +16,25 @@ struct curve
 {
   curve    *previous;
   waypoint *present;
+  bool     Junction;
   curve    *next;
+};
+
+struct junction
+{
+  waypoint *location;  // bearing is set to INVALID
+  junction *destination[4];  
+  // Where there are < 4 destinations, some pointers are NULL
+  // If there are more than 4 destinations, one of the destinations has same location and
+  // zero Distance.
+  int Distance[4];  // mm
+  int Speed[4];     // mm / s
+  curve *route[4];  // intermediate waypoints between location and Destination.
+  // location != route[n]->present
+  // The vehicle does not pass though location if it is turning.  
+  // The vehicle travels from (the last waypoint of the curve leading into the junction)
+  // to route[n]->present.
+  // When a curve reached a juntion, Curve.next == NULL
 };
 
 #define PI ((float) 3.1415925)
