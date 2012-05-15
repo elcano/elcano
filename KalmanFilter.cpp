172ddd9bc44f40324af91f26b7f930cbd7405c8c
Tyler Folsom <tyler@tfolsom.com>
2012-05-15 05:15:16 +0000

    More flexible dimensions on matrices.

--- KalmanFilter.cpp
+++ KalmanFilter.cpp
@@ -15,33 +15,44 @@ Kalman filter
 Matrix x state: longs for position_mm_east, position_mm_north,
   velocity_mm_s_east, velocity_mm_s_north
 */
-
+#define NSTATES 4
+#define NSEEN 2
 void Filter(REAL* State, // e.g  x, y position, x, y velocity
             REAL* uncertainty,   // P uncertainty covariance
             REAL* measurements, // Z: measured x and y position in mm
             REAL deltaT_s,  // time from previous state to measurement in seconds
             REAL* variance)     // variance of the measurements
 {
+      REAL state_transition[NSTATES*NSTATES]   = 
+                            {1., 0,  deltaT_s, 0,
+                             0,  1., 0,        deltaT_s,
+                             0,  0,  1,        0,
+                             0,  0,  0,        1};
       REAL state_transition[]   = {1., 0,  deltaT_s, 0,
                                    0,  1., 0,        deltaT_s,
                                    0,  0,  1,        0,
                                    0,  0,  0,        1};
       // position' = 1*position + 1*velocity
       // velocity' = 1*velocity
-      REAL observable[] = {1., 0, 0, 0,
-                           0, 1., 0, 0};
+      REAL observable[NSEEN*NSTATES]; // = {1., 0, 0, 0,
+                   //        0, 1., 0, 0};
       // only position is measurable; not velocity
-      matrix x = matrix(4,1,State); // initial state (location and velocity)
-      matrix P = matrix(4,4, uncertainty); // 4x4 initial uncertainty
-      matrix meas = matrix(2,1,measurements);
+      matrix x = matrix(NSTATES,1,State); // initial state (location and velocity)
+      matrix P = matrix(NSTATES,NSTATES, uncertainty); // 4x4 initial uncertainty
+      matrix meas = matrix(NSEEN,1,measurements);
  
-	matrix u = matrix( 4, 1); // 4x1 external motion; set to 0.
+	matrix u = matrix( NSTATES, 1); // 4x1 external motion; set to 0.
         // u comes from what the vehicle is trying to do, e.g. accelerate.
-	matrix F = matrix(4,4, state_transition); //  next state function
-	matrix H = matrix(2,4, observable); //  measurement function
-	matrix R = matrix(2,2, variance);  //  measurement variance
-	matrix I = matrix(4); // 4x4 identity matrix
-
+	matrix F = matrix(NSTATES,NSTATES, state_transition); //  next state function
+        for (int i = 0; i < NSEEN*NSTATES; i++)
+        {
+          observable[i] = i%(NSTATES+1) ? 0 : 1;
+        }
+	matrix H = matrix(NSEEN,NSTATES, observable); //  measurement function
+        H.show();
+	matrix R = matrix(NSEEN,NSEEN, variance);  //  measurement variance
+	matrix I = matrix(NSTATES); // 4x4 identity matrix
+ 
        // prediction
         matrix Fx = F * x;
 	matrix xNew = Fx + u;
