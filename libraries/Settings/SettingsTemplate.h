b99d73863d63a6089f72d9eeb32e3a37bbd9bbe3
jbobotek <jeremy.bobotek@gmail.com>
2016-06-22 17:09:18 -0700

    Removed reduntant #defines

--- libraries/Settings/SettingsTemplate.h
+++ libraries/Settings/SettingsTemplate.h
@@ -27,18 +27,31 @@ VEHICLE_NUMBER to your own new number.
 #define RC_SPEKTRUM 
 #undef  RC_HITEC
 //OUTPUT values -  0 to 255
-#define MIN_ACC_OUT 40
-#define MAX_ACC_OUT 227
+
+// MIN and MAX ACC set the minimum signal to get the motor going, and maximum allowable acceleration for the motor
+// Values (0-255) represent digital values that get convered into analog pwm signals sent to the motor
+#define MIN_ACC_OUT 40  // Throttle has no effect until 1.2 V
+#define MAX_ACC_OUT 227 // 3.63V
+
+// MIN and MAX BRAKE_OUT set values to be sent to the brake actuator that controls the brakes on the front wheels
+// Values (0-255) represent digital values that get convered into analog pwm signals
 #define MIN_BRAKE_OUT 180
 #define MAX_BRAKE_OUT 250
+
+// RIGHT, STRAIGHT, and LEFT TURN_OUT set values to be sent to the steer actuator that changes the direction of the front wheels
+// Values (0-255) represent digital values that get convered into analog pwm signals
 #define RIGHT_TURN_OUT 146
 #define LEFT_TURN_OUT 230
 #define STRAIGHT_TURN_OUT 180
+
 // Turn sensors are believed if they are in this range while wheels are straight
+// MAX values here are for the safety of the actuator so as not to break/overload it
 #define RIGHT_MIN_COUNT 80
 #define RIGHT_MAX_COUNT 284
 #define LEFT_MIN_COUNT  80
 #define LEFT_MAX_COUNT  284
+
+// Trike specific Physical perameters and pins/channels
 #define DAC_CHANNEL 0    // output to motor actuator
 #define STEER_OUT_PIN 7 // Output to steer actuator
 #define BRAKE_OUT_PIN 6  // output to brake actuator
@@ -61,17 +74,7 @@ VEHICLE_NUMBER to your own new number.
 #define RC_RDR  5
 #define RC_RVS  6
 #define NUMBER_CHANNELS 6
-//OUTPUT values -  0 to 255
-#define MIN_ACC_OUT 40
-#define MAX_ACC_OUT 227
-#define MIN_BRAKE_OUT 167
-#define MAX_BRAKE_OUT 207
-#define RIGHT_TURN_OUT 146
-#define LEFT_TURN_OUT 230
-#define STRAIGHT_TURN_OUT 187
 #define DAC_CHANNEL 0
-#define STEER_OUT_PIN 7 // Output to steer actuator
-#define BRAKE_OUT_PIN 6  // output to brake actuator
 
 #endif
 
@@ -81,18 +84,31 @@ VEHICLE_NUMBER to your own new number.
 #undef RC_SPEKTRUM 
 #define  RC_HITEC
 //OUTPUT values -  0 to 255
-#define MIN_ACC_OUT 50
+
+// MIN and MAX ACC set the minimum signal to get the motor going, and maximum allowable acceleration for the motor
+// Values (0-255) represent digital values that get convered into analog pwm signals sent to the motor
+#define MIN_ACC_OUT 40
 #define MAX_ACC_OUT 227
+
+// MIN and MAX BRAKE_OUT set values to be sent to the brake actuator that controls the brakes on the front wheels
+// Values (0-255) represent digital values that get convered into analog pwm signals
 #define MIN_BRAKE_OUT 210
 #define MAX_BRAKE_OUT 254
+
+// RIGHT, STRAIGHT, and LEFT TURN_OUT set values to be sent to the steer actuator that changes the direction of the front wheels
+// Values (0-255) represent digital values that get convered into analog pwm signals
 #define RIGHT_TURN_OUT 160 
 #define LEFT_TURN_OUT 254 
 #define STRAIGHT_TURN_OUT 192
+
 // Turn sensors are believed if they are in this range while wheels are straight
+// MAX values here are for the safety of the actuator so as not to break/overload it
 #define RIGHT_MIN_COUNT 725
 #define RIGHT_MAX_COUNT 785
 #define LEFT_MIN_COUNT  880
 #define LEFT_MAX_COUNT  940
+
+// Trike specific Physical perameters and pins/channels
 #define DAC_CHANNEL 3
 #define STEER_OUT_PIN 7 // Output to steer actuator
 #define BRAKE_OUT_PIN 9  // output to brake actuator
@@ -108,23 +124,14 @@ VEHICLE_NUMBER to your own new number.
 // Parameters used by RC_Control_interrupts
 #define RC_AUTO 1
 #define RC_ESTP 2
-#define RC_RDR  3
+#define RC_RDR  3 //rudder, side to side on left stick (not used)
 #define RC_GO   4
 #define RC_TURN 5
 #define NUMBER_CHANNELS 5
 // Controller has no channel for RC_AUTO
-#define RC_RVS  6
+#define RC_RVS  6 //reverse, not used on vehicle 2
 //OUTPUT values -  0 to 255
-#define MIN_ACC_OUT 40
-#define MAX_ACC_OUT 227
-#define MIN_BRAKE_OUT 167
-#define MAX_BRAKE_OUT 207
-#define RIGHT_TURN_OUT 146
-#define LEFT_TURN_OUT 230
-#define STRAIGHT_TURN_OUT 187
 #define DAC_CHANNEL 3
-#define STEER_OUT_PIN 7 // Output to steer actuator
-#define BRAKE_OUT_PIN 9  // output to brake actuator
 
 #endif
 
