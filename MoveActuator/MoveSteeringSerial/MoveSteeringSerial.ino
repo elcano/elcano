dac9cb09f37275f0820677481061b11ef0c1cafb
jbobotek <jeremy.bobotek@gmail.com>
2016-07-22 19:49:48 -0700

    Low Level Radio Interrupt code tweaked, Steering Test Program has had brake test code

--- MoveActuator/MoveSteeringSerial/MoveSteeringSerial.ino
+++ MoveActuator/MoveSteeringSerial/MoveSteeringSerial.ino
@@ -1,16 +1,18 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(3, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
   Serial.begin(9600);
   
 }
-int sig = 128;
+int sig = 190;
 void loop() {
   
   // put your main code here, to run repeatedly:
   if(Serial.available()){
     sig = Serial.parseInt();
     Serial.println(sig);
+    analogWrite(7, sig);
   }
-  analogWrite(3, sig);
+  //analogWrite(8, sig);
 }
