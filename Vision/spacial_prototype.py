6d7a0e0620577ac08d03b0ba48b3b71271566b4f
Dylan Katz <djshredd@gmail.com>
2016-07-27 14:17:03 -0700

    Add functions for generating perspective matricies

--- Vision/spacial_prototype.py
+++ Vision/spacial_prototype.py
@@ -3,9 +3,33 @@ import math as m
 
 # Prototype code for the image-to-world location system. Requires numpy.
 
-# TODO
-def _inverse_perspective():
-    pass
+# Generate a frustum matrix for a perspective projection
+# fov = field of view (y axis)
+# aspect = ratio of the image
+# near, far = the distance bounds
+def _perspective(fov,
+                 aspect,
+                 near,
+                 far):
+
+    assert(fov > 0 and aspect != 0)
+    h = 1 / m.tan(fov / 2.0)
+    w = h / aspect
+    q = far / (near - far)
+
+    return np.array([(w, 0, 0      , 0),
+                     (0, h, 0      , 0),
+                     (0, 0, q      , 1),
+                     (0, 0, -q*near, 0)])
+
+# Find the pseudo-inverse of a perspective matrix
+# See _perspective for more information on fov, aspect, near, and far
+def _inverse_perspective(fov,
+                         aspect,
+                         near,
+                         far):
+
+    return np.linalg.pinv(_perspective(fov, aspect, near, far))
 
 # Convert a global coordinate to a relative coordinate
 # (roll, pitch, yaw) = camera_angle
@@ -53,12 +77,27 @@ def _global_to_relative(camera_angle,
     converted_to_camera = mc.dot(mb).dot(ma).dot(df)
     return cc.dot(converted_to_camera) + im
 
+# Tiny test suite for the _perspective function
+def _test_perspective():
+    print('Testing Perspective Matrix Generation')
+    print(_perspective(0.5, 1   , 1, 50))
+    print(_perspective(1.2, 0.75, 1, 50))
+
+# Tiny test suite for the _inverse_perspective function
+def _test_inverse_perspective():
+    print('Testing Inverted Perspective Matrix Generation')
+    print(_inverse_perspective(0.5, 1   , 1, 50))
+    print(_inverse_perspective(1.2, 0.75, 1, 50))
+
 # Tiny test suite for the _global_to_relative function
 def _test_global_to_relative():
+    print('Testing Global->Relative Transformation')
     print(_global_to_relative((0, 0, 0), (0, 0, 0), (10, 2, 0), 0.01, (320, 240), (0.05, 0.05)))
     print(_global_to_relative((0, 0, 0), (0, 0, 1), (10, 2, 0), 0.01, (320, 240), (0.05, 0.05)))
     print(_global_to_relative((0, 0.3, 0), (0, 0, 0), (10, 2, 0), 0.01, (320, 240), (0.05, 0.05)))
     print(_global_to_relative((0, 0, 0.3), (0, 0, 0), (10, 2, 0), 0.01, (320, 240), (0.05, 0.05)))
 
 if __name__ == "__main__":
+    _test_perspective()
+    _test_inverse_perspective()
     _test_global_to_relative()
