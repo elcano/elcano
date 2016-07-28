9e8effff383c51017ff8b888b6eb5a3e3e6b5fae
jbobotek <jeremy.bobotek@gmail.com>
2016-07-28 16:28:19 -0700

    Updated SettingsTemplate to match writeMicroseconds variables for servos

--- libraries/Settings/SettingsTemplate.h
+++ libraries/Settings/SettingsTemplate.h
@@ -17,9 +17,9 @@ VEHICLE_NUMBER to your own new number.
 */
 
 // Dr. Folsom's orange trike.
-//#define VEHICLE_NUMBER 1
+#define VEHICLE_NUMBER 1
 // Yellow Catrike at the University of Washington at Bothell.
-#define VEHICLE_NUMBER 2
+//#define VEHICLE_NUMBER 2
 
 #if (VEHICLE_NUMBER == 1)
 
@@ -30,28 +30,20 @@ VEHICLE_NUMBER to your own new number.
 
 // MIN and MAX ACC set the minimum signal to get the motor going, and maximum allowable acceleration for the motor
 // Values (0-255) represent digital values that get convered into analog pwm signals sent to the motor
-#define MIN_ACC_OUT 40
-#define MAX_ACC_OUT 227
-<<<<<<< HEAD
+#define MIN_ACC_OUT 50
+#define MAX_ACC_OUT 235
 
 // MIN and MAX BRAKE_OUT set values to be sent to the brake actuator that controls the brakes on the front wheels
 // Values (0-255) represent digital values that get convered into analog pwm signals
-#define MIN_BRAKE_OUT 180
-#define MAX_BRAKE_OUT 250
+#define MIN_BRAKE_OUT 1750
+#define MAX_BRAKE_OUT 1300
 
 // RIGHT, STRAIGHT, and LEFT TURN_OUT set values to be sent to the steer actuator that changes the direction of the front wheels
 // Values (0-255) represent digital values that get convered into analog pwm signals
-#define RIGHT_TURN_OUT 146
-#define LEFT_TURN_OUT 230
-#define STRAIGHT_TURN_OUT 180
+#define RIGHT_TURN_OUT 1000
+#define LEFT_TURN_OUT 2000
+#define STRAIGHT_TURN_OUT 1500
 
-=======
-#define MIN_BRAKE_OUT 167
-#define MAX_BRAKE_OUT 207
-#define RIGHT_TURN_OUT 146
-#define LEFT_TURN_OUT 230
-#define STRAIGHT_TURN_OUT 188
->>>>>>> 223c88c1c7eda80690319c5b6e952afe6e00f1ee
 // Turn sensors are believed if they are in this range while wheels are straight
 // MAX values here are for the safety of the actuator so as not to break/overload it
 #define RIGHT_MIN_COUNT 80
@@ -68,7 +60,6 @@ VEHICLE_NUMBER to your own new number.
 
 // Parameters used by MoveActuator
 // external PWM output
-#define DISK_BRAKE 6
 // Use DAC A
 #define THROTTLE_CHANNEL 0
 
@@ -91,7 +82,6 @@ VEHICLE_NUMBER to your own new number.
 #undef RC_SPEKTRUM 
 #define  RC_HITEC
 //OUTPUT values -  0 to 255
-<<<<<<< HEAD
 
 // MIN and MAX ACC set the minimum signal to get the motor going, and maximum allowable acceleration for the motor
 // Values (0-255) represent digital values that get convered into analog pwm signals sent to the motor
@@ -109,15 +99,6 @@ VEHICLE_NUMBER to your own new number.
 #define LEFT_TURN_OUT 254 
 #define STRAIGHT_TURN_OUT 192
 
-=======
-#define MIN_ACC_OUT 40
-#define MAX_ACC_OUT 227
-#define MIN_BRAKE_OUT 167
-#define MAX_BRAKE_OUT 207
-#define RIGHT_TURN_OUT 146
-#define LEFT_TURN_OUT 230
-#define STRAIGHT_TURN_OUT 187
->>>>>>> 223c88c1c7eda80690319c5b6e952afe6e00f1ee
 // Turn sensors are believed if they are in this range while wheels are straight
 // MAX values here are for the safety of the actuator so as not to break/overload it
 #define RIGHT_MIN_COUNT 725
@@ -128,13 +109,13 @@ VEHICLE_NUMBER to your own new number.
 // Trike specific Physical perameters and pins/channels
 #define DAC_CHANNEL 0
 #define STEER_OUT_PIN 7 // Output to steer actuator
-#define BRAKE_OUT_PIN 9  // output to brake actuator
+#define BRAKE_OUT_PIN 8  // output to brake actuator
 #define WHEEL_DIAMETER_MM 500
 #define MOTOR_POLE_PAIRS 23
 
 // Parameters used by MoveActuator
 // external PWM output
-#define DISK_BRAKE 9
+#define DISK_BRAKE 9 //depricated, use BRAKE_OUT_PIN
 // Use DAC D
 #define THROTTLE_CHANNEL 0
 
@@ -147,13 +128,9 @@ VEHICLE_NUMBER to your own new number.
 #define RC_TURN 5
 #define NUMBER_CHANNELS 5
 // Controller has no channel for RC_AUTO
-<<<<<<< HEAD
 #define RC_RVS  6 //reverse, not used on vehicle 2
 //OUTPUT values -  0 to 255
 #define DAC_CHANNEL 0
-=======
-#define RC_RVS  6
->>>>>>> 223c88c1c7eda80690319c5b6e952afe6e00f1ee
 
 #endif
 
