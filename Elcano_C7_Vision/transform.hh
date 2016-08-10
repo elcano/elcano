4abd7ec9f5ed4ba8398199efdc69e628a3fa5903
Dylan Katz <djshredd@gmail.com>
2016-08-10 12:56:31 -0700

    Fix header formatting, add other methods

--- Elcano_C7_Vision/transform.hh
+++ Elcano_C7_Vision/transform.hh
@@ -1,19 +1,42 @@
 #pragma once
 
 #include "types.hh"
+#include <tuple>
 
 /* Transform on-camera rectangles into vectors in 3D space */
 
 namespace elcano
 {
 	double
-    compute_error(
-        std::tuple<double, double, double>       cam_angle,
-        std::tuple<uint64_t, uint64_t, uint64_t> cam_pos,
-        std::tuple<uint64_t, uint64_t, uint64_t> cone_pos,
-        std::tuple<uint64_t, uint64_t>           img_size,
-        std::tuple<uint64_t, uint64_t>           sensor_size,
-        uint64_t                                 focal_length,
-        std::tuple<double, double>               detected_loc
-    );
+	get_z(
+		uint64_t h,
+		double   fovy
+	);
+
+	std::tuple<double, double>
+	global_to_relative(
+		std::tuple<double, double, double>       cam_angle,
+		std::tuple<uint64_t, uint64_t, uint64_t> cam_pos,
+		std::tuple<uint64_t, uint64_t, uint64_t> cone_pos,
+		std::tuple<uint64_t, uint64_t>           img_size,
+		std::tuple<double, double>               sensor_size,
+		uint64_t                                 focal_length
+	);
+
+	double
+	distance_2d(
+		std::tuple<double, double> camera,
+		std::tuple<double, double> locale
+	);
+
+	double
+	compute_error(
+		std::tuple<double, double, double>       cam_angle,
+		std::tuple<uint64_t, uint64_t, uint64_t> cam_pos,
+		std::tuple<uint64_t, uint64_t, uint64_t> cone_pos,
+		std::tuple<uint64_t, uint64_t>           img_size,
+		std::tuple<double, double>               sensor_size,
+		uint64_t                                 focal_length,
+		std::tuple<double, double>               detected_loc
+	);
 }
