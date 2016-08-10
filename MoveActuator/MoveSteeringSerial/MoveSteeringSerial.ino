18c729cc8ba46bcb428dbf61acc0813229635417
poptarts <poptarts@uw.edu>
2016-08-10 15:57:32 -0700

    Created file to measure turning speed between extreme angles; removed brake references from MoveSteeringSerial (why were they there?); updated wheel diameter in settings template.

--- MoveActuator/MoveSteeringSerial/MoveSteeringSerial.ino
+++ MoveActuator/MoveSteeringSerial/MoveSteeringSerial.ino
@@ -1,16 +1,20 @@
+#include <Settings.h>
+#include <Servo.h>
+Servo steer;
+//Servo brake;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(3, OUTPUT);
+  steer.attach(7);
+  //brake.attach(6);
   Serial.begin(9600);
-  
 }
-int sig = 128;
-void loop() {
-  
+int sig = 1500;
+void loop(){
   // put your main code here, to run repeatedly:
   if(Serial.available()){
     sig = Serial.parseInt();
     Serial.println(sig);
+    steer.writeMicroseconds(sig);
+    //brake.writeMicroseconds(1500);
   }
-  analogWrite(3, sig);
 }
