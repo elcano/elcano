650caf896a985c00c83846ccc4499ff89dfe0418
jbobotek <jeremy.bobotek@gmail.com>
2016-08-12 20:54:26 -0700

    Cleaned LowLevel - RC works! MoveSteeringSerial now switches between brakes and steer

--- MoveActuator/MoveSteeringSerial/MoveSteeringSerial.ino
+++ MoveActuator/MoveSteeringSerial/MoveSteeringSerial.ino
@@ -1,20 +1,29 @@
 #include <Settings.h>
 #include <Servo.h>
-Servo steer;
+
+Servo steer, brake;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(BRAKE_OUT_PIN, OUTPUT);
+  brake.attach(BRAKE_OUT_PIN);
   steer.attach(STEER_OUT_PIN);
   Serial.begin(9600);
 }
-int sig = 1500; //195
+int sig = 1500;
+bool h = false;
 void loop() {
+  
   // put your main code here, to run repeatedly:
-  if(Serial.available())
-  {
+  if(Serial.available()){
     sig = Serial.parseInt();
     Serial.println(sig);
-    steer.writeMicroseconds(sig);
-    //analogWrite(BRAKE_OUT_PIN, sig);
-  }
+    
+    if(h){
+      steer.writeMicroseconds(sig);
+      h = false;
+    }
+    else{
+      brake.writeMicroseconds(sig);
+      h = true;
+    }
+  }  
 }
