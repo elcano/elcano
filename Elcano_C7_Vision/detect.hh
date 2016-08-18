e95bcdddb8b0a15010018209d9989dd6d45dfda2
Dylan Katz <muddmaker.97@gmail.com>
2016-08-18 16:46:00 -0700

    Added RaspPi camera functionality. Plain, unscaling cone detection implemented

--- Elcano_C7_Vision/detect.hh
+++ Elcano_C7_Vision/detect.hh
@@ -2,7 +2,13 @@
 
 /* Helper function for processing camera input */
 
+using namespace cv;
+
 namespace elcano
 {
-	void detect_and_draw(cv::Mat&, cv::CascadeClassifier&, double);
+	Mat convertToEdgeMap(Mat input, int lowThresh, int highThresh, int apertureSize);
+	Mat filterByColor(Mat input, int thresholds[], int colorType);
+	Point templateMatch(Mat image, Mat target, int matchMedthod);
+	Point templateMatchEdges(Mat image, Mat target, int lowThresh, int highThresh, int apertureSize, int matchMethod);
+	Point templateMatchBlobs(Mat image, Mat target, int thresholds[], int matchMethod);
 }
