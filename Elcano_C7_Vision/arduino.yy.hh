3c71b0a45160a1ae1e60a7a465e02857fc336157
Dylan Katz <muddmaker.97@gmail.com>
2016-09-01 13:15:48 -0700

    Document arduino.hh and transform.hh

--- Elcano_C7_Vision/arduino.yy.hh
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-typedef struct yy_buffer_state * YY_BUFFER_STATE;
-extern int yyparse(elcano::SerialData *);
-extern YY_BUFFER_STATE yy_scan_string(const char *);
-extern void yy_delete_buffer(YY_BUFFER_STATE);
