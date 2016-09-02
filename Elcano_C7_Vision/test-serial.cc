c2be8a4c19e890ca54ddaa25a049f0bc9ec0f8e9
Dylan Katz <muddmaker.97@gmail.com>
2016-09-02 15:59:30 -0700

    Update Elcano_Serial format, rewrite serial-test

--- Elcano_C7_Vision/test-serial.cc
+++ Elcano_C7_Vision/test-serial.cc
@@ -11,39 +11,32 @@ main(
 ) {
 	args::ArgumentParser parser("Test serial connection", "Connect to sockets");
 	args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
-	args::ValueFlag<std::string> in_name(parser, "dev", "The input device", {'i', "input"}, "/dev/ttyACM0");
-	args::ValueFlag<unsigned long> in_rate(parser, "rate", "The input baudrate", {'j', "in-baudrate"}, 9600ul);
-	args::ValueFlag<std::string> out_name(parser, "dev", "The output device", {'o', "output"}, "/dev/ttyACM0");
-	args::ValueFlag<unsigned long> out_rate(parser, "rate", "The output baudrate", {'p', "out-baudrate"}, 9600ul);
+	args::ValueFlag<std::string> name(parser, "dev", "The input device", {'D', "device"}, "/dev/ttyACM0");
+	args::ValueFlag<unsigned long> rate(parser, "rate", "The input baudrate", {'b', "baudrate"}, 9600ul);
+	args::ValueFlag<unsigned long> count(parser, "count", "The number of iterations", {'c', "count"}, 10ul);
+	args::ValueFlag<unsigned long> delay(parser, "delay", "The delay between serial calls (ms)", {'d', "delay"}, 100ul);
 	
 	try { parser.ParseCLI(argc, argv); }
 	catch (args::Help) { std::cout << parser; return 0; }
 	catch (args::ParseError e) { std::cerr << e.what() << std::endl << parser; return 1; }
 	
-	std::cout << "Test reading from an external device:" << std::endl;
-	{
-		serial::Serial input(args::get(in_name), args::get(in_rate), serial::Timeout::simpleTimeout(100));
-		if (!input.isOpen()) { std::cerr << "Unable to open input!" << std::endl; return 2; }
-		elcano::SerialData data;
-
-		elcano::read(input, data);
-		std::cout << data << std::endl;
-	}
+	serial::Serial device(args::get(name), args::get(rate), serial::Timeout::simpleTimeout(args::get(delay)));
+	std::chrono::duration<unsigned long, std::milli> d(args::get(delay));
 	
-	std::cout << "Test writing to an external device:" << std::endl;
+	elcano::SerialData data;
+	data.kind = elcano::MsgType::drive;
+	data.speed = 20;
+	data.angle = 60;
+	
+	for (unsigned long i = 1; i < args::get(count); ++i)
 	{
-		serial::Serial output(args::get(out_name), args::get(out_rate), serial::Timeout::simpleTimeout(100));
-		if (!output.isOpen()) { std::cerr << "Unable to open output!" << std::endl; return 3; }
+		elcano::read(device, data);
 		
-		elcano::SerialData data;
-		data.kind = elcano::MsgType::sensor;
-		data.speed = 20;
-		data.angle = 58;
-		data.posE = 1234;
-		data.posN = 4321;
+		std::cout << "parsed output: ";
+		elcano::write(std::cout, data);
+		std::cout << std::endl;
 		
-		elcano::write(output, data);
-		elcano::read(output, data);
-		std::cout << data << std::endl;
+		elcano::write(device, data);
+		std::this_thread::sleep_for(d);
 	}
 }
