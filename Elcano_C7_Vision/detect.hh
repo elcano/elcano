fd35352b34bcd3aa2e0632cec29a6fab4b23d5d3
AaronConrad <aaron.conrad.13@outlook.com>
2016-09-15 23:34:52 +0000

    Altered makerfare demo for portable usage

--- Elcano_C7_Vision/detect.hh
+++ Elcano_C7_Vision/detect.hh
@@ -9,6 +9,6 @@ namespace elcano
 	Mat convertToEdgeMap(Mat input, int lowThresh, int highThresh, int apertureSize);
 	Mat filterByColor(Mat input, int thresholds[], int colorType);
 	Point templateMatch(Mat image, Mat target, int matchMedthod, double &value);
-	Point templateMatchEdges(Mat image, Mat target, int lowThresh, int highThresh, int apertureSize, int matchMethod);
-	Point templateMatchBlobs(Mat image, Mat target, int thresholds[], int matchMethod);
+	//Point templateMatchEdges(Mat image, Mat target, int lowThresh, int highThresh, int apertureSize, int matchMethod);
+	//Point templateMatchBlobs(Mat image, Mat target, int thresholds[], int matchMethod);
 }
