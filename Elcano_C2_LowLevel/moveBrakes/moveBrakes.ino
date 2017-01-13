2f85008fc925e80367a26f35be06d51277021583
jbllona <jesse.llona@gmail.com>
2017-01-12 18:25:03 -0800

    Changed the brake_servo

--- Elcano_C2_LowLevel/moveBrakes/moveBrakes.ino
+++ Elcano_C2_LowLevel/moveBrakes/moveBrakes.ino
@@ -1,6 +1,6 @@
 #include <Settings.h>
 #include <Servo.h>
-int pin = 13;
+int pin = BRAKE_OUT_PIN;
 Servo STEER_SERVO;
 void setup() {
   STEER_SERVO.attach(pin); 
@@ -15,9 +15,10 @@ void loop() {
 
     Serial.println("ON");
     STEER_SERVO.writeMicroseconds(1515);
-//    delay(1500);
+    delay(1500);
     Serial.println("OFF");
-
+    STEER_SERVO.writeMicroseconds(1300);
+    delay(1500);
     // 1300 full off
     // 1515 full on
 }
