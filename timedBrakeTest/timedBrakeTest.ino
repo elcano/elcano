40f552f5d90467324db0be3d5b51109b24f350c9
jbllona <jesse.llona@gmail.com>
2017-01-25 14:40:03 -0800

    /fixbrakes: minor adjustments and most recent tests

--- timedBrakeTest/timedBrakeTest.ino
+++ timedBrakeTest/timedBrakeTest.ino
@@ -1,6 +1,7 @@
 #include <ElcanoTimedBrake.h>
 #include <Servo.h>
 using namespace elcano;
+using namespace privateMembers;
 void setup() 
 {
   Serial.begin(9600);
@@ -8,6 +9,27 @@ void setup()
   pinMode(6, OUTPUT); // retract
   digitalWrite(8, LOW); // extend
   digitalWrite(6, LOW); // retract
+  extend(1000);
+  retract(1000);
+  extend(1000);
+  retract(1000);
+  extend(1000);
+  retract(1000);
+  extend(1000);
+  retract(1000);
+  extend(1000);
+  retract(1000);
+  extend(1000);
+  retract(1000);
+//  setupBrake();
+//  applyBrakes();
+//  retractBrakes();
+//  applyBrakes();
+//  retractBrakes();
+//  applyBrakes();
+//  retractBrakes();
+//  applyBrakes();
+//  retractBrakes();
 }
 void loop() 
 {
