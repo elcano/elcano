ba80a9dac0ceee9ff83a8b6001970524020f2824
jbllona <jesse.llona@gmail.com>
2017-02-15 14:27:48 -0800

    C3 -> C2 comms
    Made simple test program (C3spy.ino) to send data to C2, modified C3 to use the newest version of the ElcanoSerial library, added a picture depicting RC connections, modified C2 code to recieve commands from C3

--- Elcano_C2_LowLevel/C3spy/C3spy.ino
+++ Elcano_C2_LowLevel/C3spy/C3spy.ino
@@ -6,11 +6,12 @@ ParseState ps;
 void setup() 
 {
   pinMode(16, OUTPUT);
-  Serial2.begin(baudrate);
-  Serial1.begin(baudrate);
+//  digitalWrite(16, LOW);
+  Serial.begin(9600);
+  Serial1.begin(9600);
   ps.dt = &sd;
   ps.input = &Serial1;
-  ps.output = &Serial2;
+  ps.output = &Serial1;
   ps.capture = elcano::MsgType::drive;
   sd.clear();
 }
@@ -18,6 +19,9 @@ void setup()
 void loop() 
 {
   sd.kind = MsgType::drive;
-  sd.speed_cmPs = 100;
+  sd.speed_cmPs = 50;
   sd.angle_deg = 10;
+  sd.write(&Serial1);
+//  Serial.println("here");
+  delay(10);
 }
