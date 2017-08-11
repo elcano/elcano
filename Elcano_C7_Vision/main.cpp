51ca4a824a6dc7e15a011aca0f2a1bf2a4d5bae1
TheRealFarm <k.farme@gmail.com>
2017-08-10 17:44:07 -0700

    added Kalman filter

--- Elcano_C7_Vision/main.cpp
+++ Elcano_C7_Vision/main.cpp
@@ -10,11 +10,11 @@ extern "C" {
 	#include "rs232.h"
 }
 
-#define CONE_HEIGHT		0.4953
+#define CONE_HEIGHT			0.4953
 #define CAM_CONSTANT		1583.5
 #define DIST_CONSTANT		CONE_HEIGHT * CAM_CONSTANT
 #define DEG_PER_PIXEL		0.036
-#define BUF_SIZE 		128
+#define BUF_SIZE 			128
 
 using namespace cv;
 using namespace std;
@@ -23,31 +23,122 @@ const Scalar light_green = Scalar(100, 255, 0);
 const Scalar yellow = Scalar(0, 255, 255);
 const Scalar red = Scalar(0, 0 , 255);
 int cport_nr=24; /* /dev/ttyACM0 */
-
+int imgXCenter = 0;
 
 bool isConvexHullPointingUp(vector<Point>& convexHull);
-void detectCones(Mat& imgOriginal);
+vector<pair<Rect,Point>> detectCones(Mat& imgOriginal);
 void sendToArduino(float dist, float deg);
 void receiveFromArduino();
+vector<pair<Rect,Point>> removeInnerOverlappingCones(vector<pair<Rect,Point>>& cones);
+void findClosestCone(vector<pair<Rect,Point>>& cones, KalmanFilter& kf, Mat& state, Mat& meas, bool& f);
 
 int main(int argc, char* argv[]) {
 	int bdrate=57600; /* 9600 baud */
 
 	char mode[]={'8','N','1',0}; // 8 data bits, no parity, 1 stop bit
 
-	if(RS232_OpenComport(cport_nr, bdrate, mode))
-	{
-		cout << "Can not open comport\n";
-		return 0;
-	}
+//	if(RS232_OpenComport(cport_nr, bdrate, mode))
+//	{
+//		cout << "Can not open comport\n";
+//		return 0;
+//	}
 
 	usleep(2000000);  /* waits 2000ms for stable condition */
 
+    int stateSize = 6;
+    int measSize = 4;
+
+    cv::KalmanFilter kf(stateSize, measSize);
+
+    cv::Mat state(stateSize, 1, CV_32F);  // [x,y,v_x,v_y,w,h]
+    cv::Mat meas(measSize, 1, CV_32F);    // [z_x,z_y,z_w,z_h]
+    // [E_x,E_y,E_v_x,E_v_y,E_w,E_h]
+
+    // Transition State Matrix A
+    // Note: set dT at each processing step!
+    // [ 1 0 dT 0  0 0 ]
+    // [ 0 1 0  dT 0 0 ]
+    // [ 0 0 1  0  0 0 ]
+    // [ 0 0 0  1  0 0 ]
+    // [ 0 0 0  0  1 0 ]
+    // [ 0 0 0  0  0 1 ]
+    cv::setIdentity(kf.transitionMatrix);
+
+    // Measure Matrix H
+    // [ 1 0 0 0 0 0 ]
+    // [ 0 1 0 0 0 0 ]
+    // [ 0 0 0 0 1 0 ]
+    // [ 0 0 0 0 0 1 ]
+    kf.measurementMatrix = cv::Mat::zeros(measSize, stateSize, CV_32F);
+    kf.measurementMatrix.at<float>(0) = 1.0f;
+    kf.measurementMatrix.at<float>(7) = 1.0f;
+    kf.measurementMatrix.at<float>(16) = 1.0f;
+    kf.measurementMatrix.at<float>(23) = 1.0f;
+
+    // Process Noise Covariance Matrix Q
+    // [ Ex   0   0     0     0    0  ]
+    // [ 0    Ey  0     0     0    0  ]
+    // [ 0    0   Ev_x  0     0    0  ]
+    // [ 0    0   0     Ev_y  0    0  ]	cout << "Img center = " << imgXCenter << endl;
+    // [ 0    0   0     0     Ew   0  ]
+    // [ 0    0   0     0     0    Eh ]
+    kf.processNoiseCov.at<float>(0) = 1e-2;
+    kf.processNoiseCov.at<float>(7) = 1e-2;
+    kf.processNoiseCov.at<float>(14) = 5.0f;
+    kf.processNoiseCov.at<float>(21) = 5.0f;
+    kf.processNoiseCov.at<float>(28) = 1e-2;
+    kf.processNoiseCov.at<float>(35) = 1e-2;
+
+    // Measures Noise Covariance Matrix R
+    cv::setIdentity(kf.measurementNoiseCov, cv::Scalar(1e-1));
+
 	VideoCapture capture(0);
 	Mat frame;
+	vector<pair<Rect, Point>> cones; // first - boudning rect of cone, second - center point
+	double ticks = 0;
+	bool found = false;
+
+	int notFoundCount = 0;
+	imgXCenter = (int)capture.get(CV_CAP_PROP_FRAME_WIDTH) / 2;
+
 	while (true){
+		double precTicks = ticks;
+		ticks = (double) getTickCount();
+		double dT = (ticks - precTicks) / getTickFrequency();
+
 		capture >> frame;
-		detectCones(frame);
+		if (found){
+			kf.transitionMatrix.at<float>(2) = dT;
+			kf.transitionMatrix.at<float>(9) = dT;
+			state = kf.predict();
+
+			cv::Rect predRect;
+			predRect.width = state.at<float>(4);
+			predRect.height = state.at<float>(5);
+			predRect.x = state.at<float>(0) - predRect.width / 2;
+			predRect.y = state.at<float>(1) - predRect.height / 2;
+
+			cv::Point center;
+			center.x = state.at<float>(0);
+			center.y = state.at<float>(1);
+			float distance = DIST_CONSTANT / predRect.height;
+			float degrees = DEG_PER_PIXEL * (center.x - imgXCenter);
+			cout << "Predicted distance, degrees = " << distance << ", " << degrees << endl;
+		}
+
+		cones = detectCones(frame);
+
+		if (cones.size() == 0){
+			notFoundCount++;
+			if (notFoundCount >= 20){
+				found = false;
+			}
+		}
+		else {
+			notFoundCount = 0;
+			findClosestCone(cones, kf, state, meas, found);
+		}
+
 		if (waitKey(20) == 27)
 			break;
 	}
@@ -90,13 +181,12 @@ bool isConvexHullPointingUp(vector<Point>& convexHull) {
 	return true;
 }
 
-void detectCones(Mat& imgOriginal){
+vector<pair<Rect, Point>> detectCones(Mat& imgOriginal){
 	Mat imgHSV, imgThreshLow, imgThreshHigh, imgThreshSmooth, imgCanny,
 		imgContours, imgAllConvexHulls, imgConvexHulls3to10, imgTrafficCones;
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
 	vector<pair<Rect, Point>> cones; // bounding rectangle of cone, and its center point
-	int imgXCenter = imgOriginal.cols / 2;
 
 	cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV);
 	inRange(imgHSV, Scalar(0, 145, 96), Scalar(12, 255, 255), imgThreshLow);
@@ -120,9 +210,6 @@ void detectCones(Mat& imgOriginal){
 	//imgAllConvexHulls = Mat::zeros(imgThresh.size(), CV_8UC3);
 	//imgConvexHulls3to10 = Mat::zeros(imgThresh.size(), CV_8UC3);
 	imgTrafficCones = Mat::zeros(imgThresh.size(), CV_8UC3);
-	float maxArea = INT_MIN;
-	float minDistance = INT_MAX;
-	float minDegrees = 0;
 	// drawing contours on the images is not necessary, but for visualization
 	for (size_t i = 0; i < contours.size(); i++) {
 		//drawContours(imgContours, contours, i, light_green, 1, 8, hierarchy, 0, Point());
@@ -136,52 +223,25 @@ void detectCones(Mat& imgOriginal){
 				Moments m = moments(hulls[i], false);
 				int cX = m.m10 / m.m00;
 				int cY = m.m01 / m.m00;
-				// get rectangle and draw center
+				// get rectangle and center
 				Rect rectangle = boundingRect(hulls[i]);
 				Point center = Point(cX, cY);
-				// determine distance
-				float distance = DIST_CONSTANT / rectangle.height;
-				float degrees = DEG_PER_PIXEL * (center.x - imgXCenter);
-				if (rectangle.area() > maxArea){
-					maxArea = rectangle.area();
-					minDistance = distance;
-					minDegrees = degrees;
-				}
 				//cout << "(" << center.x << ", " << center.y << ")";
 				//cout << "\tDistance, degrees: " << distance << ", " << degrees << endl;
 				cones.push_back(make_pair(rectangle,center));
 			}
 		}
 	}
-	
-	// iterate through detected cones to find the closest one, and send that information to the arduino
-	for (size_t i = 0; i < cones.size(); i++){
-		if(cones[i].first.area() == maxArea){
-			circle(imgOriginal, cones[i].second, 1, Scalar(0, 255, 0), 2);
-			// get reading from Kalman filter on where cone should be
-			sendToArduino(minDistance, minDegrees);
-		}
-		else {
-			circle(imgOriginal, cones[i].second, 1, Scalar(255, 0, 0), 2);
-		}
-	}
-	
-	if (!cones.empty()){
-		cones.clear();
-	}
-	else{ // tell Kalman filter that no cones were found in image
-		
-	}
-	receiveFromArduino();
 
 	//imshow("imgContours", imgContours);
 	imshow("imgThreshSmooth", imgThreshSmooth);
 	//imshow("imgTrafficCones", imgTrafficCones);
 	imshow("imgOriginal", imgOriginal);
-	//waitKey(0);
+
+	return cones;
 }
 
-void sendToArduino(float dist, float deg){ 
+void sendToArduino(float dist, float deg){
 	int i_deg = (int)deg+10;
 	if (i_deg < 0) i_deg = 0;
 	string send_str = to_string((int)dist) + "," + to_string(i_deg) + "\n";
@@ -199,3 +259,80 @@ void receiveFromArduino(){
 	}
 }
 
+// logic is not correct for this function yet, do not use
+vector<pair<Rect,Point>> removeInnerOverlappingCones(vector<pair<Rect,Point>>& cones){
+	vector<pair<Rect,Point>> conesWithOverlapsRemoved;
+	for (size_t i = 0; i < cones.size(); i++){
+		// if a point is inside another rectangle
+		for (size_t j = i+1; j < cones.size(); j++){
+			// if the point is between the x & y coordinates of another rectangle
+			if ((cones[i].second.x >= cones[j].first.x) &&
+					(cones[i].second.x <= (cones[j].first.x + cones[j].first.width)) &&
+					(cones[i].second.y >= cones[j].first.y) &&
+					(cones[i].second.y <= cones[j].first.y + cones[j].first.height)){
+				// return the cone that has larger area
+				if (cones[i].first.area() > cones[j].first.area()){
+					conesWithOverlapsRemoved.push_back(cones[i]);
+				}
+				else{
+					conesWithOverlapsRemoved.push_back(cones[j]);
+				}
+			}
+		}
+	}
+	return conesWithOverlapsRemoved;
+}
+
+void findClosestCone(vector<pair<Rect,Point>>& cones, KalmanFilter &kf, Mat& state, Mat& meas, bool &found){
+	pair<Rect,Point> cone;
+	float minDistance = INT_MAX;
+	float minDegrees = 0;
+	float maxArea = INT_MIN;
+
+	for (size_t i = 0; i < cones.size(); i++){
+		float distance = DIST_CONSTANT / cones[i].first.height;
+		float degrees = DEG_PER_PIXEL * (cones[i].second.x - imgXCenter);
+		if(cones[i].first.area() > maxArea){
+			maxArea = cones[i].first.area();
+			minDistance = distance;
+			minDegrees = degrees;
+			cone = cones[i];
+		}
+	}
+
+	// update measurement matrix
+	meas.at<float>(0) = cone.second.x;
+	meas.at<float>(1) = cone.second.y;
+	meas.at<float>(2) = (float)cone.first.width;
+	meas.at<float>(3) = (float)cone.first.height;
+
+	if (!found) // First detection!
+	{
+		// >>>> Initialization
+		kf.errorCovPre.at<float>(0) = 1; // px
+		kf.errorCovPre.at<float>(7) = 1; // px
+		kf.errorCovPre.at<float>(14) = 1;
+		kf.errorCovPre.at<float>(21) = 1;
+		kf.errorCovPre.at<float>(28) = 1; // px
+		kf.errorCovPre.at<float>(35) = 1; // px
+
+		state.at<float>(0) = meas.at<float>(0);
+		state.at<float>(1) = meas.at<float>(1);
+		state.at<float>(2) = 0;
+		state.at<float>(3) = 0;
+		state.at<float>(4) = meas.at<float>(2);
+		state.at<float>(5) = meas.at<float>(3);
+		// <<<< Initialization
+
+		kf.statePost = state;
+
+		found = true;
+	}
+	else { // Kalman Correction
+		kf.correct(meas);
+	}
+
+	cout << "Calculated distance, degrees = " << minDistance << ", " << minDegrees << endl;
+	//sendToArduino(minDistance, minDegrees);
+	//receiveFromArduino();
+}
