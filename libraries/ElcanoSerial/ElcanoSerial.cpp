8e204f8b1b5e54ddd456734fc590d41cef6299f8
Yoshiaki Sono <yoshiaki.sono@gmail.com>
2018-08-13 21:05:29 -0700

    update ElcanoSerial

--- libraries/ElcanoSerial/ElcanoSerial.cpp
+++ libraries/ElcanoSerial/ElcanoSerial.cpp
@@ -1,4 +1,7 @@
 #include "ElcanoSerial.h"
+#include <FastCRC.h>
+
+#define TERMINAL_CHAR '\n'
 
 // Elcano_Serial.h
 // By Dylan Katz
@@ -7,314 +10,270 @@
 // connections. Complete documentation and usage is on the wiki.
 namespace elcano {
 
-ParseStateError ParseState::update(void) {
-  int c;
-  int i = 0;
-  char buffer[MAX_MSG_SIZE];
-  
-  // Because no attribute type can be used for more than 1 flag, these
-  // variables keep track of whether the given attribute type has been used
-  // in a flag so far.
-  bool numberWasUsed = false;
-  bool speedWasUsed = false;
-  bool angleWasUsed = false;
-  bool obstacleWasUsed = false;
-  bool bearingWasUsed = false;
-  bool EposWasUsed = false;
-  bool NposWasUsed = false;
-  bool probabilityWasUsed = false;
-  
-  // Checksum variables
-  uint8_t checksum = 0; // checksum computed from message string
-  uint8_t msgChecksum = -1; // checksum included with the message
-  
-start:
-  if (i == MAX_MSG_SIZE) {
-    state = 0;
-    return ParseStateError::long_msg;
-  }
-  c = input->read();
-  buffer[i] = c;
-  i++;
-  if (c == -1) {
-    state = 0;
-    return ParseStateError::unavailable;
-  }
-  if (c == ' ' || c == '\t' || c == '\0' || c == '\r') goto start;
-  switch(state) {
-  case 0:
-    // During this state, we begin the processing of a new SerialData packet
-    // and we must receive {D,S,G,X} to state which type of packet we are
-    // working with. As an optimization, the user may set only a specific type
-    // of messages to capture, with all other types result in the stream being
-    // directly fed into the `output' serial port. Set the `capture' variable
-    // to do this, and use `|' to let it capture more than one type at once.
-    dt->clear();
-    switch(c) {
-#define STATE(CHR, TYPE)             \
-    case CHR:                        \
-      if (capture & MsgType::TYPE) { \
-        dt->kind = MsgType::TYPE;    \
-        state = 1;                   \
-      } else {                       \
-        output->print(CHR);          \
-        state = 50;                  \
-      }                              \
-      break;
-STATE('D', drive)
-STATE('S', sensor)
-STATE('G', goal)
-STATE('X', seg)
-#undef STATE
-    default:
-      mostRecent = c;
-      state = 0;
-      return ParseStateError::bad_type;
-    }
-    goto start;
-  case 1:
-    // During this state, we need to find '{' if we are reading the value for
-    // an attribute, or '\n' if we are done with the packet, or '-' or '1'-'9'
-    // if we are reading the checksum
-    switch(c) {
-    //case '\n': state = 0; return dt->verify() ? ParseStateError::success : ParseStateError::inval_comb;
-    case '\n': state =  0; return ParseStateError::success;
-    case '{' : state =  2; goto start;
-    case '1' :
-    case '2' :
-    case '3' :
-    case '4' :
-    case '5' :
-    case '6' :
-    case '7' :
-    case '8' :
-    case '9' : state = 40; checksum = CRC8.smbus((const uint8_t*)buffer, i - 1); msgChecksum = c - '0'; goto start;
-    case '-' : state = 41; checksum = CRC8.smbus((const uint8_t*)buffer, i - 1); goto start;
-    default  : state =  0; return ParseStateError::bad_lcurly;
-    }
-  case 2:
-    // During this state, we begin reading an attribute of the SerialData and
-    // we must recieve {n,s,a,o,b,r,p} to state _which_ attribute
-    switch(c) {
-    case 'n': state = 3; goto start;
-    case 's': state = 4; goto start;
-    case 'a': state = 5; goto start;
-    case 'o': state = 6; goto start;
-    case 'b': state = 7; goto start;
-    case 'r': state = 8; goto start;
-    case 'p': state = 9; goto start;
-    default : state = 0; return ParseStateError::bad_attrib;
-    }
-#define STATES(SS, PS, NS, TERM, HOME, VAR, BOOL) \
-  case SS:                                  \
-    if (BOOL) {                             \
-      state = 0;                            \
-      return ParseStateError::bad_attrib;   \
-    }                                       \
-    dt->VAR = 0;                            \
-    if (c == '-') {                         \
-      state = NS;                           \
-      goto start;                           \
-    }                                       \
-    state = PS;                             \
-  case PS:                                  \
-    if (c >= '0' && c <= '9') {             \
-      dt->VAR *= 10;                        \
-      dt->VAR += c - '0';                   \
-      goto start;                           \
-    } else if (c == TERM) {                 \
-      BOOL = true;                          \
-      state = HOME;                         \
-      goto start;                           \
-    } else {                                \
-      state = 0;                            \
-      return ParseStateError::bad_number;   \
-    }                                       \
-  case NS:                                  \
-    if (c >= '0' && c <= '9') {             \
-      dt->VAR *= 10;                        \
-      dt->VAR -= c - '0';                   \
-      goto start;                           \
-    } else if (c == TERM) {                 \
-      BOOL = true;                          \
-      state = HOME;                         \
-      goto start;                           \
-    } else {                                \
-      state = 0;                            \
-      return ParseStateError::bad_number;   \
-    }
-STATES( 3, 13, 23, '}',  1, number, numberWasUsed)
-STATES( 4, 14, 24, '}',  1, speed_cmPs, speedWasUsed)
-STATES( 5, 15, 25, '}',  1, angle_mDeg, angleWasUsed)
-STATES( 6, 16, 26, '}',  1, obstacle_mm, obstacleWasUsed)
-STATES( 7, 17, 27, '}',  1, bearing_deg, bearingWasUsed)
-STATES( 8, 18, 28, '}',  1, probability, probabilityWasUsed)
-STATES( 9, 19, 29, ',', 10, posE_cm, EposWasUsed)
-STATES(10, 20, 30, '}',  1, posN_cm, NposWasUsed)
-#undef STATES
-  case 40: // positive checksum
-    if (c == '\n') {
-      if (checksum != msgChecksum) {
-        state = 0;
-        return ParseStateError::bad_checksum;
-        } else {
-        state = 0;
-        return ParseStateError::success;
-        }
-    } else if (c < '0' || c > '9') {
-      return ParseStateError::bad_checksum;
+static FastCRC8 CRC8;
+
+static bool is_unavailable_byte(const uint8_t & c) {
+  return c == 255;
+}
+
+static MsgType parse_msg_type(const uint8_t & c) {
+  if (c == 'D') return MsgType::drive;
+  if (c == 'S') return MsgType::sensor;
+  if (c == 'G') return MsgType::goal;
+  if (c == 'X') return MsgType::seg;
+  return MsgType::none;
+}
+
+static int32_t* get_datum_ptr(const SerialData & dt, const uint8_t & datum_type) {
+  if (datum_type == 'n') return &(dt.number);
+  if (datum_type == 's') return &(dt.speed_cmPs);
+  if (datum_type == 'a') return &(dt.angle_mDeg);
+  if (datum_type == 'o') return &(dt.obstacle_mm);
+  if (datum_type == 'b') return &(dt.bearing_deg);
+  if (datum_type == 'r') return &(dt.probability);
+  if (datum_type == 'p') return &(dt.posE_cm);
+  if (datum_type == 'q') return &(dt.posN_cm);
+  return nullptr;
+}
+
+ParseStateError ParseState::update(unsigned int max_passthru_ct) {
+  dt->clear();
+
+  uint8_t c; // The temp var for the newest byte.
+
+  uint8_t buffer[MAX_MSG_SIZE];
+  size_t buffer_idx = 0;
+
+  // 1) Discard all bytes until receiving the head of a new packet.
+  //    Do this b/c, in general, the node has just finished writing a new packet,
+  //    and during this time, any incoming packet would already have been
+  //    partially discarded.
+  // 2) For all new packets, pass through all non-captured packets.
+  // 1&2) While waiting for a new packet, detect lack of packets on the wire,
+  //      so that this method can exit and give the single thread a chance to
+  //      send a new packet.
+  // Note, it is possible for a correctly formatted packet without a recipient
+  // to circulate on the wire indefinitely.
+  unsigned int passthru_ct = 0;
+  while(true) {
+    c = input->read();
+
+    if (is_unavailable_byte(c)) {
+      // Ideally we should use a threshold to recognize a few contiguous
+      // `unavailable` bytes, but an attempt to do so resulted in either hanging
+      // or perpetual `unavailable` result; further investigation is welcome.
+      // If we use a threshold, it may be good for every node to add a jitter,
+      // so that every node waits a different amount to verify the `unavailable`
+      // condition, in order to prevent nodes from continuously sending new
+      // packets at the same frequency, only to have them all discarded.
+      return ParseStateError::unavailable;
     } else {
-      msgChecksum *= 10;
-      msgChecksum += c - '0';
-      goto start;
-    }
-  case 41: // negative checksum
-    if (c == '\n') {
-      if (checksum != msgChecksum) {
-        state = 0;
-        return ParseStateError::bad_checksum;
+      if (c == TERMINAL_CHAR && passthru_ct > max_passthru_ct) {
+        output->print(c);
+        return ParseStateError::passthru;
+      }
+
+      MsgType msg_type = parse_msg_type(c);
+
+      if (msg_type == MsgType::none) {
+        // Not the head of a new packet.
+        // Either discard or pass thru.
+        if (passthru_ct > 0) {
+          output->print(c);
+        }
+      } else if (msg_type & capture) {
+        // Received the head of a captured packet.
+        buffer[buffer_idx++] = c;
+        dt->kind = msg_type;
+        // Now go parse the rest of the packet.
+        break; 
       } else {
-        state = 0;
+        // Received the head of a non-captured packet.
+        output->print(c);
+        passthru_ct++;
+      }
+    }
+  }
+
+  auto get_next_byte = [this, &buffer, &buffer_idx](uint8_t & c) {
+    do {
+      c = this->input->read();
+    } while (is_unavailable_byte(c));
+    buffer[buffer_idx++] = c;
+  };
+
+  auto abort = [&get_next_byte, &buffer_idx, &MAX_MSG_SIZE](uint8_t & c) {
+    do {
+      get_next_byte(c);
+    } while (c != TERMINAL_CHAR && buffer_idx < MAX_MSG_SIZE);
+  };
+
+  // Unlike Serial.parseInt():
+  // - allows an initial digit to be already read
+  // - does not handle negative sign
+  // - parses an int, not a long
+  auto parseInt = [&get_next_byte, &c, &buffer_idx, &MAX_MSG_SIZE](int32_t* datum_ptr, const uint8_t & stop_char) {
+    do {
+      get_next_byte(c);
+      if (c >= '0' && c <= '9') {
+        *datum_ptr = *datum_ptr * 10 + c - '0';
+      } else {
+        return c == stop_char;
+      }
+    } while (buffer_idx < MAX_MSG_SIZE);
+    return false;
+  };
+
+  // 3) Either parse a datum or validate the checksum, as determined by the next byte. Repeat.
+  while (buffer_idx < MAX_MSG_SIZE) {
+    get_next_byte(c);
+    if (c == '{') {
+      // Then, read until '}' and parse a datum.
+
+      get_next_byte(c);
+      int32_t* datum_ptr = get_datum_ptr(*dt, c);
+      if (datum_ptr == nullptr) {
+        abort(c);
+        return ParseStateError::bad_attrib;
+      }
+
+      get_next_byte(c);
+      bool is_negative;
+      if (c == '-') {
+        *datum_ptr = 0;
+        is_negative = true;
+      } else {
+        *datum_ptr = c - '0';
+        is_negative = false;
+      }
+
+      bool parse_success = parseInt(datum_ptr, '}');
+      if (! parse_success) {
+        abort(c);
+        return ParseStateError::bad_number;
+      }
+
+      if (is_negative) {
+        *datum_ptr = *datum_ptr * -1;
+      }
+
+    } else if (c >= '0' && c <= '9') {
+      // Then, read until TERMINAL_CHAR and validate the checksum.
+
+      size_t checksum_begin_idx = buffer_idx - 1;
+
+      int32_t parsed_checksum = c - '0';
+      bool parse_success = parseInt(&parsed_checksum, TERMINAL_CHAR);
+      if (! parse_success) {
+        abort(c);
+        return ParseStateError::bad_number;
+      }
+
+      uint8_t calculated_checksum = CRC8.smbus(buffer, checksum_begin_idx);
+
+      if (calculated_checksum != static_cast<uint8_t>(parsed_checksum)) {
+        return ParseStateError::bad_checksum;
+      } else if (! dt->verify()) {
+        return ParseStateError::inval_comb;
+      } else {
         return ParseStateError::success;
       }
-    } else if (c < '0' || c > '9') {
-      return ParseStateError::bad_checksum;
     } else {
-      msgChecksum *= 10;
-      msgChecksum -= c - '0';
-      goto start;
+      // Then, the packet did not follow the format of e.g.
+      // `A{b1234}{c5678}999`
+      return ParseStateError::bad_number;
     }
-  case 50:
-    output->print((char)c);
-    if (c == '\n') {
-      state = 0;
-      return ParseStateError::passthru;
-    }
-    goto start;
   }
+
+  return ParseStateError::long_msg;
 }
 
 bool SerialData::write(HardwareSerial *dev) {
-  
   char buffer[MAX_MSG_SIZE];
-  int i = 0;
-  String string;
+  size_t i = 0;
+  bool success;
   
   switch (kind) {
-  case MsgType::drive:  buffer[i++] = 'D'; break;
-  case MsgType::sensor: buffer[i++] = 'S'; break;
-  case MsgType::goal:   buffer[i++] = 'G'; break;
-  case MsgType::seg:    buffer[i++] = 'X'; break;
-  default:              return false;
+    case MsgType::drive:  buffer[i++] = 'D'; break;
+    case MsgType::sensor: buffer[i++] = 'S'; break;
+    case MsgType::goal:   buffer[i++] = 'G'; break;
+    case MsgType::seg:    buffer[i++] = 'X'; break;
+    default:              return false;
   }
-  // At this point the value of i will be 1. No bounds check necessary
-  if (number != NaN && (kind == MsgType::goal || kind == MsgType::seg)) {
+
+  auto append_datum = [&buffer, &i](const char & datum_type, const int32_t & datum) {
+    String datum_str = String(datum);
+    size_t strLen = datum_str.length();
+
+    // 3 for '{', datum_type, '}'.
+    if (i >= MAX_MSG_SIZE - 3 - strLen) {
+      return false;
+    }
+
     buffer[i++] = '{';
-    buffer[i++] = 'n';
-    buffer[i++] = ' ';
-    string = String(number);
-    int strLen = string.length();
-    for (int j = 0; j < strLen; j++) {
-      buffer[i++] = string.charAt(j);
+    buffer[i++] = datum_type;
+    for (size_t j = 0; j < strLen; j++) {
+      buffer[i++] = datum_str.charAt(j);
     }
     buffer[i++] = '}';
+
+    return true;
+  };
+
+  // At this point the value of i will be 1. No bounds check necessary
+  if (number != NaN && (kind == MsgType::goal || kind == MsgType::seg)) {
+    success = append_datum('n', number);
+    if (! success) return false;
   }
   // At this point the max value of i is 16. No bounds check necessary
   if (speed_cmPs != NaN && kind != MsgType::goal) {
-    buffer[i++] = '{';
-    buffer[i++] = 's';
-    buffer[i++] = ' ';
-    string = String(speed_cmPs);
-    int strLen = string.length();
-    for (int j = 0; j < strLen; j++) {
-      buffer[i++] = string.charAt(j);
-    }
-    buffer[i++] = '}';
+    success = append_datum('s', speed_cmPs);
+    if (! success) return false;
   }
   // At this point the max value of i is 31. No bounds check necessary
   if (angle_mDeg != NaN && (kind == MsgType::drive || kind == MsgType::sensor)) {
-    buffer[i++] = '{';
-    buffer[i++] = 'a';
-    buffer[i++] = ' ';
-    string = String(angle_mDeg);
-    int strLen = string.length();
-    for (int j = 0; j < strLen; j++) {
-      buffer[i++] = string.charAt(j);
-    }
-    buffer[i++] = '}';
+    success = append_datum('a', angle_mDeg);
+    if (! success) return false;
   }
   // At this point the max value of i is 46. No bounds check necessary
   if (obstacle_mm != NaN && kind == MsgType::sensor) {
-    buffer[i++] = '{';
-    buffer[i++] = 'o';
-    buffer[i++] = ' ';
-    string = String(obstacle_mm);
-    int strLen = string.length();
-    for (int j = 0; j < strLen; j++) {
-      buffer[i++] = string.charAt(j);
-    }
-    buffer[i++] = '}';
+    success = append_datum('o', obstacle_mm);
+    if (! success) return false;
   }
   // At this point the max value of i is 61. No bounds check necessary
   if (bearing_deg != NaN && kind != MsgType::drive) {
-    buffer[i++] = '{';
-    buffer[i++] = 'b';
-    buffer[i++] = ' ';
-    string = String(bearing_deg);
-    int strLen = string.length();
-    for (int j = 0; j < strLen; j++) {
-      buffer[i++] = string.charAt(j);
-    }
-    buffer[i++] = '}';
+    success = append_datum('b', bearing_deg);
+    if (! success) return false;
   }
   // At this point the max value of i is 91. A bounds check is necessary
+  // if (i >= MAX_MSG_SIZE) return false;
   if (probability != NaN && kind == MsgType::goal) {
-    if (i >= MAX_MSG_SIZE - 6)
-      return false;
-    buffer[i++] = '{';
-    buffer[i++] = 'r';
-    buffer[i++] = ' ';
-    string = String(probability);
-    int strLen = string.length();
-    if (i >= MAX_MSG_SIZE - strLen - 2)
-      return false;
-    for (int j = 0; j < strLen; j++)
-      buffer[i++] = string.charAt(j);
-    buffer[i++] = '}';
+    success = append_datum('r', probability);
+    if (! success) return false;
   }
   // At this point the max value of i is 76. A bounds check is necessary
+  // if (i >= MAX_MSG_SIZE) return false;
   if (posE_cm != NaN && posN_cm != NaN && kind != MsgType::drive) {
-    if (i >= MAX_MSG_SIZE - 8)
-      return false;
-    buffer[i++] = '{';
-    buffer[i++] = 'p';
-    buffer[i++] = ' ';
-    string = String(posE_cm);
-    int strLen = string.length();
-    if (i >= MAX_MSG_SIZE - strLen - 4)
-      return false;
-    for (int j = 0; j < strLen; j++)
-      buffer[i++] = string.charAt(j);
-    buffer[i++] = ',';
-    string = String(posN_cm);
-    strLen = string.length();
-    if (i >= MAX_MSG_SIZE - strLen - 2)
-      return false;
-    for (int j = 0; j < strLen; j++)
-      buffer[i++] = string.charAt(j);
-    buffer[i++] = '}';
+    success = append_datum('p', posE_cm);
+    if (! success) return false;
+    success = append_datum('q', posN_cm);
+    if (! success) return false;
   }
-  // At this point the max value of i is 106. A bounds check is necessary
+
+  // At this point the max value of i is 108. A bounds check is necessary
+  // if (i >= MAX_MSG_SIZE) return false;
+
   FastCRC8 CRC8;
   int checksum = CRC8.smbus((const uint8_t*)buffer, i);
   String chksumStr = String(checksum);
   int chksumLen = chksumStr.length();
-  if (i == MAX_MSG_SIZE - chksumLen - 1)
+  if (i == MAX_MSG_SIZE - chksumLen - 1) // 1 for TERMINAL_CHAR
     return false;
   for (int j = 0; j < chksumLen; j++)
     buffer[i++] = chksumStr.charAt(j);
-  buffer[i++] = '\n';
-  
+
+  buffer[i++] = TERMINAL_CHAR;
+
   // Write buffer to device
   for (int j = 0; j < i; j++) {
     dev->print(buffer[j]);
@@ -334,7 +293,6 @@ void SerialData::clear(void) {
   probability = NaN;
 }
 
-/*
 bool SerialData::verify(void) {
   return true;
   switch (kind) {
@@ -368,6 +326,5 @@ bool SerialData::verify(void) {
   }
   return true;
 }
-*/
 
 } // namespace elcano
--- libraries/ElcanoSerial/ElcanoSerial.h
+++ libraries/ElcanoSerial/ElcanoSerial.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <HardwareSerial.h>
-#include <FastCRC.h>
 
 // Elcano_Serial.h
 // By Dylan Katz
@@ -18,6 +17,10 @@ const int32_t baudrate = 74800;
 
 //! The maximum message size
 const int32_t MAX_MSG_SIZE = 64;
+
+//! The min continuous occurrence of the unavailable byte (255) that is
+//  interprted as the absence of any message on the wire.
+const int32_t MIN_UNAVAILABLE_CT = 0;
 
 //! The different possible types of SerialData packets
 enum class MsgType : int8_t {
@@ -51,7 +54,7 @@ struct SerialData {
 
   void clear(void);  //!< Set the values to the defaults
   bool write(HardwareSerial * /**< Connection to write to */); //!< Write to a serial connection
-  //bool verify(void); //!< Check that the types match the values
+  bool verify(void); //!< Check that the types match the values
 };
 
 //! The different possible results of the ParseState::update method
@@ -59,13 +62,11 @@ enum class ParseStateError : int8_t {
   success      = 0, //!< Complete package
   inval_comb   = 1, //!< Complete package, failed validation
   unavailable  = 2, //!< Couldn't read a complete packet from the device at this time
-  bad_type     = 3, //!< Syntax error: types should be [DSGX]
-  bad_lcurly   = 4, //!< Syntax error: expected '{' or '\n' but got neither
   bad_attrib   = 5, //!< Syntax error: attributes should be [nsaobrp] and only used once
   bad_number   = 6, //!< Syntax error: number had a bad symbol
   passthru     = 7, //!< An uncaptured message has been passed through
   bad_checksum = 8, //!< Error in checksum
-  long_msg     = 9  //!< Message is too long
+  long_msg     = 9 //!< Message is too long
 };
 
 //! Contains internal state for the SerialData parser.
@@ -74,11 +75,9 @@ struct ParseState {
   HardwareSerial *input;  //!< Connection to read from
   HardwareSerial *output; //!< Connection to write to
   MsgType capture;        //!< MsgType(s) to capture
-  char mostRecent;
-  ParseStateError update(void); //!< Update the state of the parser based on a single character
-private:
-  uint8_t state = 0;   //!< Internal state variable
-  FastCRC8 CRC8;
+
+  //!< Update the state of the parser based on a single character
+  ParseStateError update(unsigned int max_passthru_ct = 50);
 };
 
 } // namespace elcano
