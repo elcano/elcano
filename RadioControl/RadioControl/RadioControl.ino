a36e4963a39a3e8e972d77b05b03da199d3e9459
JBreithaupt <joseph.breithaupt@gmail.com>
2018-08-22 17:43:22 -0700

    Adding support for Spektrum ESTOP signal; the Spektrum receiver is not currently working so this has not yet been tested live.

--- RadioControl/RadioControl/RadioControl.ino
+++ RadioControl/RadioControl/RadioControl.ino
@@ -1,11 +1,14 @@
 #include "Settings.h"
-#include <RH_ASK.h> // external library
 #include "ElcanoSerial.h"
+#include <RH_ASK.h> // external library
 using namespace elcano;
 
 /* be sure to check this before compiling a sketch!
   TRANSMITTER 1 = transmitter mode, runs on Micro or other boards
-  TRANSMITTER 0 = receiver mode, does NOT work on a Micro */
+  TRANSMITTER 0 = receiver mode, does NOT work on a Micro.
+  This is a source of ElcanoSerial.write() problems because Serial/Serial#
+  is not always a HardwareSerial pointer.
+*/
 #define TRANSMITTER 0
 #if TRANSMITTER
 #define RECEIVER 0
@@ -13,10 +16,9 @@ using namespace elcano;
 #define RECEIVER 1
 #endif
 
-const bool DEBUG = false; // verbose debugging, turn this off for faster execution time
+const bool DEBUG = false; // serial monitor debugging, turn this off for faster execution time
 
-struct Inputs
-{
+struct Inputs {
   long throttle;
   long angle;
   bool ebrakeOn;
@@ -33,8 +35,6 @@ void waitInputPWM(int timeout);
 void waitInputByte(int timeout);
 void sendElcanoSerial(void);
 void debugPacket(unsigned long timer);
-
-
 
 #define ESERIAL Serial1
 #define PC Serial
@@ -57,7 +57,7 @@ static bool using_us_units = false; // we use byte values(0-255) or microseconds
 
 RH_ASK driver;
 /* default config:
-RH_ASK(uint16_t speed = 2000, uint8_t rxPin = 11, uint8_t txPin = 12, uint8_t pttPin = 10, bool pttInverted = false); */
+  RH_ASK(uint16_t speed = 2000, uint8_t rxPin = 11, uint8_t txPin = 12, uint8_t pttPin = 10, bool pttInverted = false); */
 
 /* LEDs
   analogWrite works on pins 3, 5*, 6*, 9, 10, and 11 of a Micro. * = higher duty cycle */
@@ -70,45 +70,44 @@ const uint8_t RESERVED_PIN = 10;    // this is the enable transmitter pin in the
 const uint8_t TX_LED_TRANSMIT = 11; // D11 is also radiohead RX pin but we are not using it on the transmitter
 const uint8_t RX_LED_NOSIGNAL = 12; // D60 is A6 on the Mega
 
-void setupReceiver()
-{
+void setupReceiver() {
   driver.init();
   driver.setModeRx();
   PC.begin(9600);
 }
 
-void waitInputByte(int timeout)
-{
-  if (DEBUG) {PC.println(F("waiting for radio packet..."));}
+void waitInputByte(int timeout) {
+  if (DEBUG) {
+    PC.println(F("waiting for radio packet..."));
+  }
   inputData.newData = false;
   static uint8_t buf[MSG_SIZE] = {0};
   uint8_t buflen = sizeof(buf);
-  do
-  {
-    if (driver.recv(buf, &buflen))
-    {
-      if (DEBUG) {PC.print(millis());}
-      if (DEBUG) {PC.println(F(" packet found"));}
-      // we do have a valid packet
+  do {
+    if (driver.recv(buf, &buflen)) {
+      if (DEBUG) {
+        PC.print(millis());
+      }
+      if (DEBUG) {
+        PC.println(F(" packet found"));
+      }
+      // we do have a valid packet so we pull out the data and set newData
       inputData.throttle = buf[0];
       inputData.angle = buf[1];
       inputData.autoOn = bitRead(buf[2], 1);
       inputData.ebrakeOn = bitRead(buf[2], 2);
       inputData.newData = true;
     }
-    if (timeout > 0)
-    {
+    if (timeout > 0) {
       delay(1);
       timeout--;
     }
   } while (timeout > 0);
 }
 
-void waitInputPWM(int timeout)
-{
+void waitInputPWM(int timeout) {
   inputData.newData = false;
-  for (int i = 0; i < RC_NUM_SIGNALS; i++)
-  {
+  for (int i = 0; i < RC_NUM_SIGNALS; i++) {
     RC_rise[i] = INVALID_DATA;
     RC_elapsed[i] = INVALID_DATA;
     RC_Done[i] = 0;
@@ -117,14 +116,20 @@ void waitInputPWM(int timeout)
   attachInterrupt(digitalPinToInterrupt(IRPT_GO), ISR_GO_rise, RISING);
   attachInterrupt(digitalPinToInterrupt(IRPT_ESTOP), ISR_ESTOP_rise, RISING);
   attachInterrupt(digitalPinToInterrupt(IRPT_BRAKE), ISR_BRAKE_rise, RISING);
-  do
-  {
-    if (RC_Done[RC_ESTP] == true)
-    {
-      // TODO need to check which interrupts are actually used, may need more ISRs
+  do {
+    if (RC_Done[RC_ESTP] == true) {
+      // we now have the pulse length so we read that in
+
+      const int THRESHOLD = 2000; // TODO: need to check this value when receiver is working
+
+      inputData.ebrakeOn = (RC_elapsed[RC_ESTP] > THRESHOLD ? true : false);
+      // now we reset everything so we can pick up new pulses, if any, while we wait
+      RC_Done[RC_ESTP] = false;
+      attachInterrupt(digitalPinToInterrupt(IRPT_ESTOP), ISR_ESTOP_rise, RISING);
+
+      // TODO: add other pins when receiver is working
     }
-    if (timeout > 0)
-    {
+    if (timeout > 0) {
       delay(1);
       timeout--;
     }
@@ -133,40 +138,35 @@ void waitInputPWM(int timeout)
 
 void loopReceiver()
 {
-  if (DEBUG) {PC.println(F("start loop"));}
+  if (DEBUG) {
+    PC.println(F("start loop"));
+  }
   int timeout = 100;
   inputData.newData = false;
-  if (using_us_units)
-  {
+  if (using_us_units) {
     waitInputPWM(timeout); // check for new data and wait for timeout
   }
-  else
-  {
+  else {
     waitInputByte(timeout); // check for new data and wait for timeout
   }
-  if (inputData.newData)
-  {
+  if (inputData.newData) {
     sendElcanoSerial();
   }
 }
 
-void sendElcanoSerial()
-{
+void sendElcanoSerial() {
   SerialData command;
   command.kind = MsgType::drive;
-  if (!using_us_units)
-  {
+  if (!using_us_units) {
     const int T_OFFSET = 0; // throttle trim
     inputData.throttle -= T_OFFSET;
     const int A_OFFSET = 3; // angle trim
     inputData.angle -= A_OFFSET;
     const int DEADZONE = 2;
-    if (abs(inputData.throttle) < DEADZONE)
-    {
+    if (abs(inputData.throttle) < DEADZONE) {
       inputData.throttle = 0;
     }
-    if (abs(inputData.angle) < DEADZONE)
-    {
+    if (abs(inputData.angle) < DEADZONE) {
       inputData.angle = 0;
     }
   }
@@ -178,18 +178,21 @@ void sendElcanoSerial()
 #if RECEIVER
   /*this #if is necessary to compile transmitter code for an Arduino Micro
     without splitting code into two files.
-	SerialData.write() requires a pointer to a HardwareSerial type (like "Serial") but
-	the Arduino Micro does not allow HardwareSerial on "Serial", only "Serial1" */
+    SerialData.write() requires a pointer to a HardwareSerial type (like "Serial") but
+    the Arduino Micro does not allow HardwareSerial on "Serial", only "Serial1" */
   command.write(&ESERIAL);
-  if (DEBUG) {PC.print(millis());}
-  if (DEBUG) {PC.println(F(" sent ES packet: "));}
+  if (DEBUG) {
+    PC.print(millis());
+  }
+  if (DEBUG) {
+    PC.println(F(" sent ES packet: "));
+  }
 #endif
 }
 
-void debugPacket(int throttle, int angle, int data, unsigned long timer)
-{
-	// this is useful for identifying problems at the transmitter
-	char separator[] = F(",\t");
+void debugPacket(int throttle, int angle, int data, unsigned long timer) {
+  // this is useful for identifying problems at the transmitter
+#define separator F(",\t")
   PC.print(timer, DEC);
   PC.print(separator);
   PC.print(throttle, DEC);
@@ -199,35 +202,27 @@ void debugPacket(int throttle, int angle, int data, unsigned long timer)
   PC.println(data, DEC);
 }
 
-void setup()
-{
-  if (TRANSMITTER)
-  {
+void setup() {
+  if (TRANSMITTER) {
     setupTransmitter();
   }
-  else
-  {
+  else {
     setupReceiver();
   }
 }
 
-void loop()
-{
-  if (TRANSMITTER)
-  {
+void loop() {
+  if (TRANSMITTER) {
     loopTransmitter();
   }
-  else
-  {
+  else {
     loopReceiver();
   }
 }
 
-void setupTransmitter()
-{
+void setupTransmitter() {
   PC.begin(9600);
-  if (DEBUG)
-  {
+  if (DEBUG) {
     PC.println(F("SystemTime(ms), throttle(byte), turn(byte), data(byte)"));
   }
   driver.init();
@@ -244,8 +239,7 @@ void setupTransmitter()
   pinMode(TX_LED_TRANSMIT, OUTPUT);
 }
 
-void loopTransmitter()
-{
+void loopTransmitter() {
   unsigned long start = millis();
   digitalWrite(TX_LED_POWER, HIGH);
   uint16_t turn = analogRead(A1) >> 2;
@@ -272,76 +266,65 @@ void loopTransmitter()
   analogWrite(TX_LED_THROTTLE, throttleBrightness);
   int turnBrightness = turn;
   analogWrite(TX_LED_TURN, turnBrightness);
-  if (DEBUG)
-  {
+  if (DEBUG) {
     debugPacket(msg[0], msg[1], msg[2], millis());
   }
   unsigned long finish = millis();
   const int LOOPTIME_MS = 100;
   unsigned long thisLoopTime = finish - start;
   unsigned long timeToDelay = LOOPTIME_MS - thisLoopTime;
-  if (timeToDelay > 0)
-  {
+  if (timeToDelay > 0) {
     delay(timeToDelay);
   }
 }
 
-void ISR_TURN_rise()
-{
+void ISR_TURN_rise() {
   noInterrupts();
   ProcessRiseOfINT(RC_TURN);
   attachInterrupt(digitalPinToInterrupt(IRPT_TURN), ISR_TURN_fall, FALLING);
   interrupts();
 }
-void ISR_BRAKE_rise()
-{
+void ISR_BRAKE_rise() {
   noInterrupts();
   ProcessRiseOfINT(RC_BRAKE);
   attachInterrupt(digitalPinToInterrupt(IRPT_BRAKE), ISR_BRAKE_fall, FALLING);
   interrupts();
 }
-//Should be bound to the red switch
-void ISR_ESTOP_rise()
-{
+void ISR_ESTOP_rise() { //Should be bound to the red switch
   noInterrupts();
   ProcessRiseOfINT(RC_ESTP);
   attachInterrupt(digitalPinToInterrupt(IRPT_ESTOP), ISR_ESTOP_fall, FALLING);
   interrupts();
 }
 
-void ISR_GO_rise()
-{
+void ISR_GO_rise() {
   noInterrupts();
   ProcessRiseOfINT(RC_GO);
   attachInterrupt(digitalPinToInterrupt(IRPT_GO), ISR_GO_fall, FALLING);
   interrupts();
 }
-void ISR_GO_fall()
-{
+void ISR_GO_fall() {
   noInterrupts();
   ProcessFallOfINT(RC_GO);
   RC_Done[RC_GO] = 1;
   attachInterrupt(digitalPinToInterrupt(IRPT_GO), ISR_GO_rise, RISING);
   interrupts();
 }
-void ISR_TURN_fall()
-{
+void ISR_TURN_fall() {
   noInterrupts();
   ProcessFallOfINT(RC_TURN);
   RC_Done[RC_TURN] = 1;
   attachInterrupt(digitalPinToInterrupt(IRPT_TURN), ISR_TURN_rise, RISING);
   interrupts();
 }
-void ISR_BRAKE_fall()
-{
+void ISR_BRAKE_fall() {
   noInterrupts();
   ProcessFallOfINT(RC_BRAKE);
   RC_Done[RC_BRAKE] = 1;
   attachInterrupt(digitalPinToInterrupt(IRPT_BRAKE), ISR_BRAKE_rise, RISING);
   interrupts();
 }
-void ISR_ESTOP_fall()
-{
+void ISR_ESTOP_fall() {
   noInterrupts();
   ProcessFallOfINT(RC_ESTP);
   RC_Done[RC_ESTP] = 1;
