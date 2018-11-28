d28f3cdf7826a6f97e478863f769130337a3684c
Taylorllippert <taylorllippert@hotmail.com>
2018-11-28 11:42:44 -0800

    Forgot to save

--- Elcano_C2_LowLevel/Vehicle.cpp
+++ Elcano_C2_LowLevel/Vehicle.cpp
@@ -1,73 +1,37 @@
 #include "trike_settings.h"
-#include "pin_settings.h"
-#include <Servo.h>
-#include "history.h"
-#include "PID_v1.h" 
-
+#include "Brakes.h"
+#include "ThrottleController.h"
 #include <Arduino.h>
 #include "Vehicle.h"
 
-#define IRQ_NONE 0
-#define IRQ_FIRST 1
-#define IRQ_SECOND 2
-#define IRQ_RUNNING 3
-#define NO_DATA 0xffffffff
 
-Vehicle::Vehicle(){
-	distance_mm = 0;
+Vehicle::Vehicle():
+	throttle(MIN_ACC_OUT,MAX_ACC_OUT,PID_CALCULATE_TIME,SelectAB,SelectCD,DAC_CHANNEL)
+{
 	
-}
-void Vehicle :: initialize() {
-	int MinTickTime_ms = WHEEL_CIRCUM_MM * 1000.0 / MAX_SPEED_mmPs;
-	int MaxTickTime_ms = WHEEL_CIRCUM_MM * 1000.0 / MIN_SPEED_mmPs;
-	int TickTime = millis();
-	int OldTick = TickTime;
-	InterruptState = 0;
-	history.oldSpeed_mmPs = history.olderSpeed_mmPs = NO_DATA;
 	
 }
 
-void Vehicle::update() {
-	brake.Check();
-	//computeSpeed(&history);
-	//computeAngle();
-	
-
-}
 
 Vehicle::~Vehicle()
 {
 }
 
-double Vehicle::getAngle() {
-	return aSensor.getAngle();
-}
-
-void Vehicle :: move(double Angle, double speed, double distance){
-
-	brake.Release();
-}
-
-void Vehicle :: compRevolution() {
-	noInterrupts();
-	unsigned long tick = millis();
-	if (InterruptState != IRQ_RUNNING) {
-		InterruptState++;
-	}
-	if (tick - TickTime > MinTickTime_ms) {
-		OldTick = TickTime;
-		TickTime = tick;
-	}
-	interrupts();
-}
-
-double Vehicle::getSpeed() {
-	
-
-}
-
-void Vehicle :: stop(){
+void Vehicle::eStop() {
 	brake.Stop();
+	throttle.stop();
 }
 
+void Vehicle::move(double dAngle, double dSpeed) {
+	if (dSpeed < (throttle.getSpeedInput_mmPs() + 10))
+		brake.Stop();
+	else {
+		brake.Release();
+		throttle.setDesiredSpeed__mmPs(dSpeed);
+	}
+	//steer.setDesiredTurn(dAngle);
+}
 
+void Vehicle::update() {
+	brake.Check();
+}
