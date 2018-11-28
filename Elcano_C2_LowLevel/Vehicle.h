e91ee8df31881a168deff7c88f43c675b7c7dca7
Taylorllippert <taylorllippert@hotmail.com>
2018-11-28 11:42:44 -0800

    Forgot to save

--- Elcano_C2_LowLevel/Vehicle.h
+++ Elcano_C2_LowLevel/Vehicle.h
@@ -1,41 +1,17 @@
 #pragma once
-#include "AngleSensor.h"
-#include "Brakes.h"
-#include "SteeringController.h"
-#include "ThrottleController.h"
-class Vehicle
-{
-public:
-	//Sensors
-	AngleSensor aSensor;
-	Brakes brake = Brakes();
-	volatile byte InterruptState;
-	volatile unsigned long TickTime = 0;
-	volatile unsigned long OldTick = 0;
-	unsigned long MinTickTime_ms;
-	unsigned long MaxTickTime_ms;
-	float SpeedCyclometer_revPs = 0.0;
-	hist history;
 
-	int calibratedWheelMaxLeft_us; // CALIBRATED_WHEEL_MAX_LEFT_US
-	int calibratedWheelStraight_us; // CALIBRATED_WHEEL_STRAIGHT_US
-	int calibratedWheelMaxRight_us; // CALIBRATED_WHEEL_MAX_RIGHT_US
-
-	int calibratedWheelSensorMaxLeft; // CALIBRATED_WHEEL_MAX_LEFT_US
-	int calibratedWheelSensorStraight;
-	int calibratedWheelSensorMaxRight; // CALIBRATED_WHEEL_MAX_RIGHT_US
+class Vehicle{
+private:
+	Brakes brake;
+	ThrottleController throttle;
 
 public:
+	double distance_mm;
 	Vehicle();
 	~Vehicle();
+	void eStop();
+	void move(double, double);
 	void update();
-	double getAngle();
-	double getSpeed();
-	void initialize();
-	void move(double Angle, double speed, double distance);
-	void stop();
-	int SteeringPID(int desiredValue);
-	void compRevolution();
-	long distance_mm = 0;
+	double getSpeed() { return throttle.getSpeedInput_mmPs(); };
 	};
 
