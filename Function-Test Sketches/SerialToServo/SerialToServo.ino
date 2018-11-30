2dc159e6e8d131490025575f67e3c150026e58f9
Ben Rockhold <ben.rockhold@gmail.com>
2018-11-30 15:18:27 -0800

    added "secret mode" to the servo test sketch

--- Function-Test Sketches/SerialToServo/SerialToServo.ino
+++ Function-Test Sketches/SerialToServo/SerialToServo.ino
@@ -12,13 +12,26 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
+    // "secret" feature; go to the full range of positions slowly
+    if (Serial.peek() == 's') {
+      Serial.readString();
+      uint8_t pos = 45;
+      int8_t delta = 2;
+      for (uint8_t count = 0; count < 150; count++) {
+        pos += delta;
+        if (pos > 142 || pos < 45) delta = -delta;
+        pos = constrain(pos, 45, 142);
+        actuator.write(pos);
+        delay(100);
+      }
+      actuator.write(94);
+      return;
+    }
+    
     uint8_t extension = Serial.parseInt();
     extension = constrain(extension, 0, 180);
     Serial.readString(); // clear serial
     Serial.print("Using extension: "); Serial.println(extension);
-    
-    
-
     actuator.write(extension);
   }
 } 
