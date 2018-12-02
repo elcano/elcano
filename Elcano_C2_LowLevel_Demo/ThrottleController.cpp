db9dda52c298d1929ce4fec078a1961a3819e150
Taylor Brady <taylorllippert@hotmail.com>
2018-12-01 17:05:40 -0800

    Fixed dac error

--- Elcano_C2_LowLevel_Demo/ThrottleController.cpp
+++ Elcano_C2_LowLevel_Demo/ThrottleController.cpp
@@ -38,11 +38,15 @@ ThrottleController::~ThrottleController()
 255 counts = 4.08 V
 */
 void ThrottleController::engageThrottle(int input) {
+  if (input != 0){
+    input = map(input, 0, MAX_SPEED_mmPs, MIN_ACC_OUT, MAX_ACC_OUT);
+  }
+  
 	if (input != currentThrottlePWM) {
 		write(DAC_CHANNEL, input);
 		currentThrottlePWM = input;  // Remember most recent throttle PWM value.
 		Serial.print("Throttle: ");
-		Serial.println(currentThrottlePWM);
+		Serial.println(input);
 	}
 }
 
@@ -56,70 +60,20 @@ void ThrottleController::ThrottlePID(int desiredValue) {
 
 void ThrottleController::write(int address, int value) {
 
-	/*-------------------------------------DAC_Write-------------------------------------------*/
-	/* DAC_Write applies value to address, producing an analog voltage.
-	  // address: 0 for chan A; 1 for chan B; 2 for chan C; 3 for chan D
-	  // value: digital value converted to analog voltage
-	  // Output goes to mcp 4802 Digital-Analog Converter Chip via SPI
-	  // There is no input back from the chip.
-
-	  REGISTER 5-3: WRITE COMMAND REGISTER FOR MCP4802 (8-BIT DAC)
-	  A/B — GA SHDN D7 D6 D5 D4 D3 D2 D1 D0 x x x x
-	  bit 15 bit 0
-	  bit 15 A/B: DACA or DACB Selection bit
-	  1 = Write to DACB
-	  0 = Write to DACA
-	  bit 14 — Don’t Care
-	  bit 13 GA: Output Gain Selection bit
-	  1 = 1x (VOUT = VREF * D/4096)
-	  0 = 2x (VOUT = 2 * VREF * D/4096), where internal VREF = 2.048V.
-	  bit 12 SHDN: Output Shutdown Control bit
-	  1 = Active mode operation. VOUT is available.
-	  0 = Shutdown the selected DAC channel. Analog output is not available at the channel that was shut down.
-	  VOUT pin is connected to 500 k (typical)
-	  bit 11-0 D11:D0: DAC Input Data bits. Bit x is ignored.
-	  With 4.95 V on Vcc, observed output for 255 is 4.08V.
-	  This is as documented; with gain of 2, maximum output is 2 * Vref
-	*/
-
-	int byte1 = ((value & 0xF0) >> 4) | 0x10; // active mode, bits D7-D4
-	int byte2 = (value & 0x0F) << 4; // D3-D0
-	SPI.beginTransaction(SPISettings(14000000,MSBFIRST,SPI_MODE0));
-	if (address < 2)
-	{
-		// take the SS pin low to select the chip:
+	int byte1 = ((value & 0xF0) >> 4) | 0x10; 
+	int byte2 = (value & 0x0F) << 4;
+  if (address == 0){
 		digitalWrite(SelectAB, LOW);
-		if (address >= 0)
-		{
-			if (address == 1)
-			{
-				byte1 |= 0x80; // second channnel
-			}
-			
 			SPI.transfer(byte1);
 			SPI.transfer(byte2);
-		}
 		// take the SS pin high to de-select the chip:
 		digitalWrite(SelectAB, HIGH);
-	}
-	else
-	{
-		// take the SS pin low to select the chip:
-		digitalWrite(SelectCD, LOW);
-		if (address <= 3)
-		{
-			if (address == 3)
-			{
-				byte1 |= 0x80; // second channnel
-			}
-			SPI.transfer(byte1);
-			SPI.transfer(byte2);
-		}
-		// take the SS pin high to de-select the chip:
-		digitalWrite(SelectCD, HIGH);
-	}
-	SPI.endTransaction();
+  }
+	
 }
+
+
+
 
 void ThrottleController::stop() {
 	engageThrottle(0);
