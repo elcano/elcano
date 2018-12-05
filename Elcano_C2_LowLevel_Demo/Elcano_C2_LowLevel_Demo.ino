a2b093c6ba17c27ec8e5481fa91d0c61adaf97ee
Taylorllippert <taylorllippert@hotmail.com>
2018-12-04 21:27:46 -0800

    fixed bad late night code

--- Elcano_C2_LowLevel_Demo/Elcano_C2_LowLevel_Demo.ino
+++ Elcano_C2_LowLevel_Demo/Elcano_C2_LowLevel_Demo.ino
@@ -3,53 +3,75 @@
 
 
 Vehicle myTrike  = Vehicle();
+
+//Timing stuff
 #define LOOP_TIME_MS 100
-unsigned long SectionStart;
-int section = 0;
 unsigned long nextTime;
 unsigned long endTime;
 unsigned long delayTime;
 #define ULONG_MAX 0x7FFFFFFF
 
+//Code for demo
+unsigned long SectionStart;
+int section = 0;
+
+//Do we want to print to the serial port?
+//Is a define so that it is constant in all code
+#define SerialPrint false
 
 void setup() {
-  
-Serial.begin(115200);
+
+Serial.begin(baud);
+myTrike.initialize();
+
+//for demo
 SectionStart = floor(millis()/1000); //seconds
 section = 0;  
-myTrike.initialize();
-Serial.println("Setup complete");
 
+if (SerialPrint)
+	Serial.println("Setup complete");
 }
 
-
 void loop() {
+//Timing code
   nextTime = nextTime + LOOP_TIME_MS;
   unsigned long timeStart_ms = millis();
-  static long int desired_speed_cmPs, desired_angle;
-  static bool e_stop = 0;
+
+//Checks if the brakes are on too long
+//If enabled computes the current angle and speed
   myTrike.update();
+
+
+/***********START OF DEMO SECTION**********************************/
+  //local vars 
+  static long int desired_speed_cmPs, desired_angle;
   int secs = floor(timeStart_ms / 1000) - SectionStart;
     
-  
+//Each section is 5 seconds
   if (secs >= 5){
-desired_angle = ChooseAngle(section);
-    desired_speed_cmPs = ChooseSpeed(section);
+	  if (section < 2) {
+// wait for 10 sec before starting the routine
+	}
+    else if(section < 9){
+//dancing section
+    desired_angle = ChooseAngle(section-2);
+    desired_speed_cmPs = ChooseSpeed(section-2);
     myTrike.noPID(desired_angle, desired_speed_cmPs);  
-
-
+    }
+    else if (section == 9)
+//reduces speed, argument is by how quickly large numbers stop quicker
+    myTrike.stop(0.25);
     
     section++;
-    //Serial.println(section);
     SectionStart= floor(timeStart_ms / 1000);
   }
-  if(section > 10)
-  section=0;
+  //once we run the routine and have probably stopped, exit
+  if (section > 15)
+	  exit(1);
+  /***********END OF DEMO SECTION**********************************/
 
 
-
-
-  
+//Timing code
    endTime = millis();
  delayTime = 0UL;
   if((nextTime >= endTime) && (((endTime < LOOP_TIME_MS)&&(nextTime < LOOP_TIME_MS)) || ((endTime >= LOOP_TIME_MS) && (nextTime >= LOOP_TIME_MS)))){
@@ -66,9 +88,36 @@ desired_angle = ChooseAngle(section);
 
 }
 
+/*
+for demo: 
+speeds up and then default is 8
+*/
+int ChooseSpeed(int section){
+    int s = 0;
+  switch(section){
+    case 0:
+      s = 2;
+      break;
+     case 1:
+      s = 4;
+      break;
+     case 2:
+      s = 6;
+      break;
+     default:
+      s = 8;
+  }
 
+  s = s* KmPh_mmPs;
+  return s;
+}
 
-
+/*
+for demo: goes 
+straight, left, left,
+straight, right, right
+default is straight
+*/
 int ChooseAngle(int section){
   int angle = 0;
   switch(section){
@@ -91,31 +140,4 @@ int ChooseAngle(int section){
   }
   return angle;
   
-}
-
-
-int ChooseSpeed(int section){
-    int s = 0;
-  switch(section){
-    case 0:
-      s = 10;
-      break;
-     case 1:
-     case 2:
-      s = 5;
-      break;
-     case 3:
-      s = 10;
-      break;
-     case 4:
-     case 5:
-      s = 5;
-      break;
-     default:
-      s = 0;
-  }
-  Serial.print("Speed: ");
-  Serial.println(s);
-  s = s* KmPh_mmPs;
-  return s;
 }
--- Elcano_C2_LowLevel_Demo/SteeringController.h
+++ Elcano_C2_LowLevel_Demo/SteeringController.h
@@ -10,26 +10,14 @@ class SteeringController{
 	double PIDSteeringOutput_us;
 	double desiredTurn_us;
 	int currentSteeringUS =0;
-
-
+	void turnOn(double input);
 	void SteeringPID(int input);
 public:
 	SteeringController();
 	~SteeringController();
-  void initialize();
+	void initialize(double input);
+	void engageSteering(double input);
 	void updateAngle(double input) { steerAngleUS = input; };
 	void setDesiredTurn(int input) { SteeringPID(input); };
-  void engageSteering(double input){
-    if(input > MAX_TURN)
-      input = MAX_TURN;
-    else if (input < MIN_TURN)
-      input = MIN_TURN;
-    if (currentSteeringUS != input){
-      Serial.print("Steering: ");
-      Serial.println(input);
-      Steer_Servo.write(input);
-      currentSteeringUS = input;
-    }
-  }
-  ;
+	
 };
