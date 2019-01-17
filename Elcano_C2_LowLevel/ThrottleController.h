9ebe040efc7caa6ba6b1579331bf8c8c3658ecce
Taylor Brady <taylorllippert@hotmail.com>
2019-01-16 16:22:52 -0800

    Made recommended changes
    Notes on Low Level Code 
    
    Good organization with Vehicle, Brake, ThrottleController etc. classes. 
    
    The only thing in the main loop is vehicle.update, which checks the brakes, computes the speed and the steering angle.  It does not set the throttle, so there is no way to make the trike go. It does not set the steering angle or apply the brakes.  How is the vehicle controlled?  Where is communication with the Remote Control or High Level? 
    
    I added the existing communication code from the elcano/elcano repo but this is the section we will be reworking for can 
    
    There are two reasons to avoid doubles. I would prefer to see doubles eliminated from the code, except where you need to interface to a library function. 
    
    On most Arduinos (Due excepted) double and float are both 32 bits.  Be honest that you are just using 32 bits. 
    
    The Atmel AVR does not have hardware floating point. It can do arithmetic with a long int much more quickly than it can manipulate a float. Almost all calculations can be done with ints (paying attention to underflow and overflow). 
    
    I replaced most of the doubles, the PID requires the type double for some of it’s inputs. We can mess with that lib b 
    
    Not having PID is OK for an interim solution, but not acceptable for the final product. There must be speed control. It does not have to be PID, but there must be a control method that sets the throttle to maintain a desired speed. 
    
    The PID controlls have not been tested but they can be enabled by using the myTrike.move instead of myTrike.noPID 
    
    Lines 38 and 39 of Vehicle.cpp: 
    
    if (dSpeed < (throttle.getSpeedInput_mmPs() + 10))  // should it be >   ? 
    
    brake.Stop(); 
    
    This is weird code that existed previously, and I would like to ask where the condition came from as dSpeed is the desired speed in cmPs and getSpeedInput is the last measured speed in mmPs 
    
    In Elcano_C2_LowLevel, WheelRev is an ISR.  It does a call to Vehicle.tick which calls thottleControl.tick. These calls add to the time in the ISR. The ticks in the Vehicle and ThrottleController classes are not obviously interrupt routines.  I would prefer to have WheelRev do its business directly without the extra calls.  tickTime_ms needs to be volatile. 
    
    I have changed tickTime_ms to be volatile. I am not sure how to eliminate the additional calls. I can eliminate the one inside throttle by making the tickTime_ms array public but making the entire throttle object public within vehicle feels like a bad design choice.  
    
     
    
    These two settings files are obsolete and have been removed. 
    
    Trike_settings.h: 
    
    There are multiple units being used for the steer and brake actuator. 
    
    The _OUT numbers are 0 to 255, intended for an Arduino PWM signal to control the actuators.  
    
    The _COUNT numbers are in ms. 
    
    The _US should be microseconds and are not compatible with _OUT 
    
    Lines 13-15 for the brake actuators are obsolete. 
    
    Lines 5-32 should be eliminated. 
    
    Lines 51-53 should be moved to pin_settings.h 
    
    Pin_settings.h: 
    
    The analog output is on lines 6,15,35-37 and should be grouped together. 
    
    Eliminate lines 9-14. 
    
    The IRPT_ numbers belong to the RC receiver; eliminate lines 16-33.

--- Elcano_C2_LowLevel/ThrottleController.h
+++ Elcano_C2_LowLevel/ThrottleController.h
@@ -6,34 +6,34 @@
 
 class ThrottleController{
 private:
-	int  currentThrottlePWM;
+	int32_t  currentThrottlePWM;
 	double speedCyclometerInput_mmPs = 0;
 	double PIDThrottleOutput_pwm;
 	double desiredSpeed_mmPs = 0;
 
 	PID speedPID;
 
-	const unsigned long MIN_TICK_TIME_ms = (WHEEL_CIRCUM_MM * 1000) / MAX_SPEED_mmPs;
-	const unsigned long MAX_TICK_TIME_ms = (WHEEL_CIRCUM_MM * 1000) / MIN_SPEED_mmPs;
+	const uint32_t MIN_TICK_TIME_ms = (WHEEL_CIRCUM_MM * 1000) / MAX_SPEED_mmPs;
+	const uint32_t MAX_TICK_TIME_ms = (WHEEL_CIRCUM_MM * 1000) / MIN_SPEED_mmPs;
 	
-	unsigned long tickTime_ms[2];
-	unsigned long calcTime_ms[2];
-	double prevSpeed_mmPs;
+	volatile uint32_t tickTime_ms[2];
+	uint32_t calcTime_ms[2];
+	int32_t prevSpeed_mmPs;
 	
-	void write(int address, int value);
-	void ThrottlePID(int desiredValue);
-	double extrapolateSpeed();
+	void write(int32_t address, int32_t value);
+	void ThrottlePID(int32_t desiredValue);
+	int32_t extrapolateSpeed();
 
 public:
 	ThrottleController();
 	~ThrottleController();
     void initialize();
 	void stop();
-	void stop(double strength);
-    void engageThrottle(int input);
-	double getSpeedInput_mmPs() { return speedCyclometerInput_mmPs; };
-	double setSpeedInput_mmPs(double speedIn) {speedCyclometerInput_mmPs = speedIn;};
-	void setDesiredSpeed__mmPs(int input) { ThrottlePID(input); };
+	void stop(int32_t strength);
+    void engageThrottle(int32_t input);
+	int32_t getSpeedInput_mmPs() { return speedCyclometerInput_mmPs; };
+	int32_t setSpeedInput_mmPs(int32_t speedIn) {speedCyclometerInput_mmPs = speedIn;};
+	void setDesiredSpeed__mmPs(int32_t input) { ThrottlePID(input); };
 	void updateSpeed();
-	void tick(unsigned long tick);
+	void tick(uint32_t tick);
 };
--- Elcano_C2_LowLevel/Vehicle.h
+++ Elcano_C2_LowLevel/Vehicle.h
@@ -9,17 +9,18 @@ private:
 	Brakes brake;
 	ThrottleController throttle;
 	SteeringController steer;
-	long computeAngleLeft();
-	long computeAngleRight();
+	int32_t computeAngleLeft();
+	int32_t computeAngleRight();
 public:
 	Vehicle();
 	~Vehicle();
 	void initialize();
 	void eStop();
-	void stop(double strength);
-	void move(double, double);
-    void noPID(double,double);
+	void stop(int32_t strength);
+	void move(int32_t, int32_t);
 	void update();
-	double getSpeed() { return throttle.getSpeedInput_mmPs(); };
+	int32_t getSpeed() { return throttle.getSpeedInput_mmPs(); };
 	void tick() { throttle.tick(millis()); };
+	
+	void noPID(int32_t,int32_t);
 };
