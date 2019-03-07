f461978673a3f52fa583577d8fca0725dd8816f6
Taylor Brady <taylorllippert@hotmail.com>
2019-03-06 16:48:32 -0800

    integrated CAN

--- Elcano_C2_LowLevel/Elcano_C2_LowLevel.ino
+++ Elcano_C2_LowLevel/Elcano_C2_LowLevel.ino
@@ -23,19 +23,7 @@ void setup() {
   if (DEBUG) {
     Serial.println("Setup complete");
   }
-  /***********START OF Communication Section********************************/
-  // CAN TEST START HERE
-  Serial.begin(115200);
-  while (CAN_OK != CAN.begin(CAN_500KBPS))
-  {
-	  if (DEBUG) {
-		  Serial.println("CAN BUS Shield init fail");
-	  }
-    delay(1000);
-  }
-  if(DEBUG)
-		Serial.println("CAN BUS init ok!");
-  /***********END OF Communication Section**********************************/
+  
 }
 
 void loop()
@@ -44,56 +32,7 @@ void loop()
   nextTime = nextTime + LOOP_TIME_MS;
   uint32_t timeStart_ms = millis();
 
-  
-
-
-  /***********START OF Communication Section********************************/
-  //This is where the communication section goes.
-  //Copied communication code from elcano/elcano/
-  static int32_t desired_speed_cmPs, desired_angle;
-  static bool e_stop = false, auto_mode = false;
-
-  if (auto_mode) {
-    unsigned char len = 0;
-    unsigned char buf[8];
-
-    if (CAN_MSGAVAIL == CAN.checkReceive())
-    {
-      CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
-      unsigned int canId = CAN.getCanId();
-
-      if (canId == Drive_CANID) { // the drive ID receive from high level
-		  if (DEBUG) {
-			  Serial.print("RECEIVE DRIVE CAN MESSAGE FROM HIGH LEVEL WITH ID: ");
-			  Serial.println(canId, HEX);
-		  }
-        int low_result = (unsigned int)(buf[3] << 24) | (buf[2] << 16) | (buf[1] << 8) | (buf[0]);
-        desired_speed_cmPs = low_result;
-		if (DEBUG) {
-			Serial.print("Speed: ");
-			Serial.print(low_result, DEC);
-		}
-
-        int high_result = (unsigned int)(buf[7] << 24) | (buf[6] << 16) | (buf[5] << 8) | (buf[4]);
-        desired_angle = high_result;
-		if(DEBUG){
-        Serial.print("  Angle: ");
-        Serial.println(high_result, DEC);
-		}
-      }
-    }
-  }
-
-  //Updated emergency brake section
-  if (e_stop)
-    myTrike.eStop();
-  else {
-    if (!auto_mode && desired_speed_cmPs < -10)
-      myTrike.eStop();
-    else
-      myTrike.update(desired_angle, desired_speed_cmPs);
-  }
-  /***********END OF Communication Section**********************************/
+  myTrike.update();
 
   //Timing code
   endTime = millis();
--- Elcano_C2_LowLevel/Vehicle.cpp
+++ Elcano_C2_LowLevel/Vehicle.cpp
@@ -5,6 +5,9 @@
 #include <PinChangeInterrupt/src/PinChangeInterrupt.h>
 #endif
 
+#define Drive_CANID 0X05
+#define EStop_CANID 0x00
+#define FEEDBACKLOW_CANID 0x0A
 volatile int32_t Vehicle::desired_speed_cmPs;
 volatile int32_t Vehicle::desired_angle;
 
@@ -25,6 +28,21 @@ void Vehicle::initialize() {
 	steer.initialize();
 	desired_angle = 0;
 	desired_speed_cmPs = 0;
+	/***********START OF Communication Section********************************/
+  // CAN TEST START HERE
+  Serial.begin(115200);
+  while (CAN_OK != CAN.begin(CAN_500KBPS))
+  {
+	  if (DEBUG) {
+		  Serial.println("CAN BUS Shield init fail");
+	  }
+    delay(1000);
+  }
+  if(DEBUG)
+		Serial.println("CAN BUS init ok!");
+  /***********END OF Communication Section**********************************/
+	if (IRPT_ESTOP)
+		attachPCINT(digitalPinToPCINT(IRPT_ESTOP), eStop, RISING);
 	if (IRPT_CAN)
 		attachPCINT(digitalPinToPCINT(IRPT_CAN), recieveCan, RISING);
 }
@@ -50,6 +68,12 @@ void Vehicle::update(int32_t tempDspeed, int32_t tempDangle) {
 Checks if the brakes have been on too long
 Computes the speed and angle
 */
+typedef union{
+		struct{
+			uint32_t sspeed;
+			uint32_t angle;
+		};
+	}speedAngleMessage;
 void Vehicle::update() {
 	int32_t tempDspeed;
 	int32_t tempDangle;
@@ -67,7 +91,14 @@ void Vehicle::update() {
 	else 
 		brake.Release();
 
-	//send msg to can
+	if(currnetSpeed< 0)
+		return;
+	else{
+		speedAngleMessage MSG;
+		MSG.sspeed = currentSpeed;
+		MSG.angle = currentAngle;
+		CAN.sendMsgBuf(FEEDBACKLOW_CANID, 0,8, (uint8_t*)&MSG);
+	}
 }
 
 
@@ -75,7 +106,38 @@ void Vehicle::update() {
 
 void Vehicle::recieveCan() {
 	noInterrupts();
-	//recieve message
+	unsigned char len = 0;
+    unsigned char buf[8];
+	
+	//estop can message
+	
+    if (CAN_MSGAVAIL == CAN.checkReceive())
+    {
+      CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+      unsigned int canId = CAN.getCanId();
+
+      if (canId == Drive_CANID) { // the drive ID receive from high level
+		  if (DEBUG) {
+			  Serial.print("RECEIVE DRIVE CAN MESSAGE FROM HIGH LEVEL WITH ID: ");
+			  Serial.println(canId, HEX);
+		  }
+        int low_result = (unsigned int)(buf[3] << 24) | (buf[2] << 16) | (buf[1] << 8) | (buf[0]);
+        desired_speed_cmPs = low_result;
+		if (DEBUG) {
+			Serial.print("Speed: ");
+			Serial.print(low_result, DEC);
+		}
+
+        int high_result = (unsigned int)(buf[7] << 24) | (buf[6] << 16) | (buf[5] << 8) | (buf[4]);
+        desired_angle = high_result;
+		if(DEBUG){
+			Serial.print("  Angle: ");
+			Serial.println(high_result, DEC);
+		}
+		if(canId == EStop_CANID){
+			eStop();
+      }
+    }
 
 	interrupts();
 }
